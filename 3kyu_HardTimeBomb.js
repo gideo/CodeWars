c6a1ae55700c995361c93a3c5f4b4dc41ba7a1df
Tong Lin <brooklyntechlin@gmail.com>
2016-11-29 21:32:27 -0500

    added 3kyu Kata Hard Time Bomb

---
+++ 3kyu_HardTimeBomb.js
@@ -0,0 +1,9 @@
+// 3 kyu - Hard Time Bomb
+// A bomb has been set to go off! You have to find the wire and cut it in order to stop the timer. 
+//There is a global var that holds the numeric ID to which wire to cut. Find that and then you can Bomb.CutTheWire(wireKey);
+
+for(var name in this) {
+  if((name.toString()).match(/boom\d/)) {
+    Bomb.CutTheWire(this[name]);
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# CodeWars
-Repository of all my Katas.
+This is a repository of the katas I have done in CodeWars and my answers.
