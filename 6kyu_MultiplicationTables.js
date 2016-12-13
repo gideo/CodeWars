d5ff85d2c3fa56830f8c4ae5e74976145d43d50f
Tong Lin <brooklyntechlin@gmail.com>
2016-12-12 22:47:07 -0500

    added 6kyu Multiplication Table

---
+++ 6kyu_MultiplicationTables.js
@@ -0,0 +1,27 @@
+// 6 kyu - Multiplication Table
+
+// Description:
+
+// Create a function that accepts dimensions, of Rows x Columns, as parameters in order to 
+// create a multiplication table sized according to the given dimensions. **The return value 
+// of the function must be an array, and the numbers must be Fixnums, NOT strings.
+
+// Example:
+
+// multiplication_table(3,3)
+
+// 1 2 3
+// 2 4 6
+// 3 6 9
+
+// -->[[1,2,3],[2,4,6],[3,6,9]]
+
+Each value on the table should be equal to the value of multiplying the number in its first row times the number in its first column.
+
+let multiplicationTable = function(row, col) {
+  let arr = [];
+  for(let num = 1; num <= row; num+=1) {
+    arr.push(Array(col).fill(0).map((_,idx) => (idx+1)*num));
+  }
+  return arr;
+}
