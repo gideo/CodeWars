b1e2576b29505bc054b327d585025e93b89c1439
Tong Lin <brooklyntechlin@gmail.com>
2016-12-28 18:08:49 -0500

    added 6 kyu Write number in expanded form

---
+++ 6kyu_WriteNumberInExpandedForm.js
@@ -0,0 +1,20 @@
+// 6 kyu - Write number in expanded form
+
+//Description:
+
+// Write Number in Expanded Form
+
+// You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+// expandedForm(12); // Should return '10 + 2'
+// expandedForm(42); // Should return '40 + 2'
+// expandedForm(70304); // Should return '70000 + 300 + 4'
+// NOTE: All numbers will be whole numbers greater than 0.
+
+const expandedForm = n => n.toString()
+                            .split("")
+                            .reverse()
+                            .map( (a, i) => a * Math.pow(10, i))
+                            .filter(a => a > 0)
+                            .reverse()
+                            .join(" + ");
