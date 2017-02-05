829109b5cb79924a7db3d88752d0fa14199f65b4
Tong Lin <brooklyntechlin@gmail.com>
2017-02-05 18:36:37 -0500

    added 6 kyu Valid Phone Number

---
+++ 6kyu_ValidPhoneNumber.js
@@ -0,0 +1,21 @@
+// 6 kyu - Valid Phone Number
+
+// Description:
+
+// Write a function that accepts a string, and returns true if it is in the form 
+// of a phone number. 
+// Assume that any integer from 0-9 in any of the spots will produce a valid 
+// phone number.
+
+// Only worry about the following format:
+// (123) 456-7890 (don't forget the space after the close parentheses) 
+
+// Examples:
+
+// validPhoneNumber("(123) 456-7890")  =>  returns true
+// validPhoneNumber("(1111)555 2345")  => returns false
+// validPhoneNumber("(098) 123 4567")  => returns false
+
+function validPhoneNumber(phoneNumber){
+  return /^(\([0-9]{3}\)\s[0-9]{3}\-[0-9]{4})$/.test(phoneNumber);
+}
