c21b665d96da3eb0bf3c03b6c141c70c7cc59923
Tong Lin <brooklyntechlin@gmail.com>
2017-02-20 17:33:00 -0500

    added 6 kyu Reversed Words

---
+++ 6kyu_ReversedWords.js
@@ -0,0 +1,15 @@
+// 6 kyu - Reversed Words
+
+// Description:
+
+// Complete the solution so that it reverses all of the words within the string 
+// passed in.
+
+// Example:
+
+// reverseWords("The greatest victory is that which requires no battle")
+// should return "battle no requires which that is victory greatest The"
+
+function reverseWords(str){
+  return str.split(" ").reverse().join(" ");
+}
