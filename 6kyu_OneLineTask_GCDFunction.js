c5b879d1007ba685c248d78a2e543894efa8f453
Tong Lin <brooklyntechlin@gmail.com>
2017-03-09 00:19:24 -0500

    added 6kyu One Line Task GCD Function

---
+++ 6kyu_OneLineTask_GCDFunction.js
@@ -0,0 +1,7 @@
+// 6kyu - One Line Task: GCD Function
+
+// Description:
+
+// TASK:Complete the GCD function in one line and less than 75 characters.
+
+GCDMeansGreatestCommonDivisorOrHighestCommonFactor=f=(a,b)=>!b?a:f(b,a%b)
