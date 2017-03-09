395cd214732c61a4b4e58d9dba5ab78e2bb4335f
Tong Lin <brooklyntechlin@gmail.com>
2017-03-08 19:40:14 -0500

    added 5kyu One Line Task nth Row Sum

---
+++ 5kyu_OneLineTask_nthRowSum.js
@@ -0,0 +1,10 @@
+// Description:
+
+// Task
+
+// I have designed a number pyramid which is shown below, your task is to find the sum 
+// of the elements in the nth row and your code should less than 23 characters(trust me, 
+// 	it's possible to code ;-)).
+// http://i.imgur.com/tzYVdIR.png
+
+nthRowSum=n=>3*n--*n+1
