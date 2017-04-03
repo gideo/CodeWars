41ef760ed136a56f4132b076bdcf5c56d5734b1f
Tong Lin <brooklyntechlin@gmail.com>
2017-04-03 12:38:36 -0400

    added 8k Convert Boolean Values to String

---
+++ 8kyu_ConvertBooleanValuestoString.js
@@ -0,0 +1,11 @@
+// 8 kyu - Convert boolean values to strings 'Yes' or 'No'
+
+// Description:
+// Complete the bool_to_word (Javascript: boolToWord ) method.
+
+// Given: a boolean value
+// Return: a 'Yes' string for true and a 'No' string for false
+
+function boolToWord( bool ){
+  return bool ? 'Yes' : 'No';
+}
