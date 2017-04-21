a4ac8a09cc3d15b0c5a29b3ff221c04d58c58a47
Tong Lin <brooklyntechlin@gmail.com>
2017-04-21 14:29:39 -0400

    added 6kyu Stop gninnipS My sdroW!

---
+++ 6kyu_StopgninnipSMysdroW.js
@@ -0,0 +1,19 @@
+// 6 kyu - Stop gninnipS My sdroW!
+
+// Description:
+
+// Write a function that takes in a string of one or more words, and returns the same 
+// string, but with all five or more letter words reversed (Just like the name of this 
+// Kata). Strings passed in will consist of only letters and spaces. Spaces will be 
+// included only when more than one word is present.
+
+
+// Examples:
+
+// spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
+// spinWords( "This is a test") => returns "This is a test" 
+// spinWords( "This is another test" )=> returns "This is rehtona test"
+
+let spinWords = function(str) {
+  return str.split(" ").map(a => (a.length >= 5) ? a.split("").reverse().join(""):a).join(" ");
+}
