7b843f731616ff7b58cd68fe7edf72d9b1ec1ce4
Tong Lin <brooklyntechlin@gmail.com>
2017-04-26 14:21:06 -0400

    added 6kyu One Line Task Largest Rectangle

---
+++ 6kyu_OneLineTask_LargestRectangle.js
@@ -0,0 +1,19 @@
+// 6kyu - One Line Task: Largest Rectangle
+// Description:
+
+// Task
+
+// Among all rectangles that have a perimeter of p, find the dimensions of the one 
+// whose area is the largest. Returns its height and width as [height,width].
+
+// Code Limit
+
+// Less than 26 characters.
+
+// Example
+
+// For p = 100, the output should be [25,25].
+
+// For p = 10, the output should be [2.5,2.5].
+
+rectanglePair=p=>[p/=4,p]
