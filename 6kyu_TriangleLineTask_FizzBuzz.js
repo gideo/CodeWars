48f1a754a0f980ae18d45be6a6433cb2539f9ec7
Tong Lin <brooklyntechlin@gmail.com>
2017-06-08 00:15:56 -0400

    added 6kyu Triangle Line Task Fizz Buzz

---
+++ 6kyu_TriangleLineTask_FizzBuzz.js
@@ -0,0 +1,41 @@
+// 6kyu - Triangle Line Task: Fizz Buzz
+
+// You need to complete the FizzBuzz function:
+
+// if n is divisible by 15, return 'fizzbuzz'
+// if n is divisible by 5, return 'buzz'
+// if n is divisible by 3, return 'fizz'
+// otherwise, return n as a number
+// where n is a positive integer.
+
+// Requirement:
+
+// 1.Every line must more than the next line 1 character.
+
+// 2.The last line should be 1 character.
+
+// 3.Spaces is not allowed.
+
+// 4.Your code should looks like a triangle:
+
+// codecode
+// odecode
+// decode
+// ecode
+// code
+// ode
+// de
+// e
+
+f=n=>n%15==0
+?"fizzbuzz"
+:(n%5===0)
+?("buzz")
+:n%3===0
+?"fizz"
+:n//uh
+//hhh
+//hh
+/*1
+*/
+;
