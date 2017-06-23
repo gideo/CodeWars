9be8cc0a0651ab9b7797fa522a56cf6e7083a5e0
Tong Lin <brooklyntechlin@gmail.com>
2017-06-23 00:02:30 -0400

    added 6kyu Hidden Key

---
+++ 6kyu_HiddenKey.js
@@ -0,0 +1,9 @@
+// 6kyu - Hidden Key
+
+// #hidden key You must find the key, there's no time to waste!
+
+// Don't forget console.log can be your friend and your enemy...
+
+// GET THAT KEY!
+
+const __KEY__ = '<!--F*&DFN7gdh7*(D^G-->';
