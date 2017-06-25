c689ecc055594e8eceaadccdc0b510243fd17540
Tong Lin <brooklyntechlin@gmail.com>
2017-06-25 00:02:19 -0400

    added 7kyu Oh come on! Hello World! Once again?

---
+++ 7kyu_OhComeOn_HelloWorld_OnceAgain.js
@@ -0,0 +1,9 @@
+// 7 kyu - Oh come on! Hello world! Once again?
+
+// Write a function f (in C# F) that returns Hello, world! string. But there is one 
+// thing, you cannot use helowrd letters.
+
+// The C# test cases ignore checking for banned letters in words class, string and 
+// public. Have fun!
+
+f=_=>'\u0048\u0065\u006C\u006C\u006F\u002C\u0020\u0077\u006F\u0072\u006C\u0064\u0021';
