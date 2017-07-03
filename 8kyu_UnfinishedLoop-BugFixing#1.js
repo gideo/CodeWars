778272204e5f449d5240c30af6e7c50aa65a8a57
Tong Lin <brooklyntechlin@gmail.com>
2017-07-03 00:21:56 -0400

    added 8kyu Unfinished Loop - Bug Fixing #1

---
+++ 8kyu_UnfinishedLoop-BugFixing#1.js
@@ -0,0 +1,16 @@
+// 8kyu - Unfinished Loop - Bug Fixing #1
+
+// Unfinished Loop - Bug Fixing #1
+
+// Oh no, Timmy's created an infinite loop! Help Timmy find 
+// and fix the bug in his unfinished For Loop!
+
+function createArray(number){
+  var newArray = [];
+  
+  for(var counter = 1; counter <= number; counter++){
+    newArray.push(counter);
+  }
+  
+  return newArray;
+}
