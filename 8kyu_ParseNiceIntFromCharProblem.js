cfff8ebdda9fc0b90ac3f31df04052b9cc468975
Tong Lin <brooklyntechlin@gmail.com>
2017-08-31 00:21:15 -0400

    added 8kyu Parse Nice Int from char problem

---
+++ 8kyu_ParseNiceIntFromCharProblem.js
@@ -0,0 +1,13 @@
+// 8kyu - Parse nice int from char problem
+
+// Ask a small girl - "How old are you?". She always says strange 
+// things... Lets help her!
+
+// For correct answer program should return int from 0 to 9.
+
+// Assume test input string always valid and may look like "1 year old" or 
+// "5 years old", etc.. The first char is number only.
+
+function getAge(inputString){
+  return +inputString[0];
+}
