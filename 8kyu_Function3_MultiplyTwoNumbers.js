50fd2eb69a2d5c77061b60ce600465f716f23833
Tong Lin <brooklyntechlin@gmail.com>
2017-09-04 00:16:12 -0400

    added 8kyu Function 3 Multiply two Numbers

---
+++ 8kyu_Function3_MultiplyTwoNumbers.js
@@ -0,0 +1,6 @@
+// 8 kyu - Function 3 - Multiplying two numbers
+
+// This function has to be called multiply and needs to take two numbers as 
+// arguments, and has to return the multiplication of the two arguments.
+
+let multiply = (a,b) => a * b;
