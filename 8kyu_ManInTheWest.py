27d887d85bfb683d607931cfc74e4785b273fbc7
Tong Lin <brooklyntechlin@gmail.com>
2017-09-24 00:02:33 -0400

    added 8kyu - Man in the West

---
+++ 8kyu_ManInTheWest.py
@@ -0,0 +1,11 @@
+// 8kyu - Man in the west
+
+// A western man is tring to find gold in a river. To do that, he passes a bucket 
+// through the river's soil and then checks if it contains any gold. However, he could
+// be more productive if he wrote an algorithm to do the job for him.
+
+// So, you need to check if there is gold in the bucket, and if so, return True/true. 
+// If not, return False/false.
+
+def check_the_bucket(bucket):
+    return 'gold' in bucket
