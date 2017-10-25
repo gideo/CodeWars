bb6a042512f3c4bb3a3518988cb7930aa3053993
Tong Lin <brooklyntechlin@gmail.com>
2017-10-25 00:05:43 -0400

    added 8kyu - Merge Two Sorted Arrays Into One

---
+++ 8kyu_MergeTwoSortedArraysIntoOne.js
@@ -0,0 +1,32 @@
+// 8kyu - Merge Two Sorted Arrays into One
+
+
+// You are given two sorted arrays that both only contain integers. Your task is to 
+// find a way to merge them into a single one, sorted in asc order. Complete the 
+// function mergeArrays(arr1, arr2), where arr1 and arr2 are the original sorted arrays.
+
+// You don't need to worry about validation, since arr1 and arr2 must be arrays with 
+// 0 or more Integers. If both arr1 and arr2 are empty, then just return an empty array.
+
+// Note: arr1 and arr2 may be sorted in different orders. Also arr1 and arr2 may have 
+// same integers. Remove duplicated in the returned result.
+
+// Example:
+
+// arr1 = [1,2,3,4,5];
+// arr2 = [6,7,8,9,10];
+// mergeArrays(arr1, arr2);  // [1,2,3,4,5,6,7,8,9,10];
+
+// arr3 = [1,3,5,7,9];
+// arr4 = [10,8,6,4,2];
+// mergeArrays(arr3, arr4);  // [1,2,3,4,5,6,7,8,9,10];
+
+// arr5 = [1,3,5,7,9,11,12];
+// arr6 = [1,2,3,4,5,10,12];
+// mergeArrays(arr5, arr6);  // [1,2,3,4,5,7,9,10,11,12];
+// Happy coding!
+
+function mergeArrays(arr1, arr2) {
+  let temp = arr1.concat(arr2);
+  return temp.sort((b, c)=> b - c).filter((a, i) => temp.indexOf(a) === i);
+}
