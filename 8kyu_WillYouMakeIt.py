940b4b2eb7ce3d665e0751539a810f469177caf3
Tong Lin <brooklyntechlin@gmail.com>
2017-10-26 00:14:11 -0400

    added 8kyu - Will You Make it?

---
+++ 8kyu_WillYouMakeIt.py
@@ -0,0 +1,12 @@
+# 8kyu - Will you make it?
+
+# Will you make it?
+
+# You were camping with your friends far away from home, but when it's time to go back, 
+# you realize that you fuel is running out and the nearest pump is 50 miles away! You 
+# know that on average, your car runs on about 25 miles per gallon. There are 2 gallons 
+# left. Considering these factors, write a function that tells you if it is possible to 
+# get to the pump or not. Function should return true if it is possible and false if not.
+
+def zeroFuel(distance_to_pump, mpg, fuel_left):
+    return mpg * fuel_left >= distance_to_pump
