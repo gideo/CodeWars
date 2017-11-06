b19dc6fa23b0fa5e86760b318cc01d009ab77b65
Tong Lin <brooklyntechlin@gmail.com>
2017-11-06 07:08:24 -0500

    added 8kyu - You only need one

---
+++ 8kyu_YouOnlyNeedOne.js
@@ -0,0 +1,10 @@
+// 8kyu - You only need one
+
+// You will be given an array (a) and a value (x). All you need to do is check whether 
+// the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either. Return true if the array 
+// contains the value, false if not.
+
+
+function check(a,x){ return a.indexOf(x) > -1 };
