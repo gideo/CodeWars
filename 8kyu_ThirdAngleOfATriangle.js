0a8340d33afbd97787597e2c766509277b88f723
Tong Lin <brooklyntechlin@gmail.com>
2017-11-09 00:15:47 -0500

    added 8kyu - Third Angle of a Triangle

---
+++ 8kyu_ThirdAngleOfATriangle.js
@@ -0,0 +1,11 @@
+// 8kyu - Third Angle of a Triangle
+
+// You are given two angles (in degrees) of a triangle.
+
+// Write a function to return the 3rd.
+
+// Note: only positive integers will be tested.
+
+function otherAngle(a, b) {
+  return 180 - a - b;
+}
