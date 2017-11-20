637d88d17e7fdc3250a909473e8b63063d2ae027
Tong Lin <brooklyntechlin@gmail.com>
2017-11-20 00:01:43 -0500

    added 8kyu - 5 without number

---
+++ 8kyu_5WithoutNumbers.py
@@ -0,0 +1,14 @@
+// 8kyu - 5 without numbers!!
+
+
+// Just for laugh Write a function that always return 5
+
+// Sounds easy right?? just bear in mind that you can't use any character 
+
+// among: 0123456789*+-/
+
+// Good luck :)
+
+
+def unusual_five():
+    return len("five!")
