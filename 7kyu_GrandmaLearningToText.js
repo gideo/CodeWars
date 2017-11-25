df3a71cf9acbf87b81a083f10a3ad07a1b85e9e9
Tong Lin <brooklyntechlin@gmail.com>
2017-11-25 00:02:22 -0500

    added 7kyu - Grandma Learning to Text

---
+++ 7kyu_GrandmaLearningToText.js
@@ -0,0 +1,14 @@
+// 7kyu - Grandma Learning to Text
+
+// Write a function that replaces 'two', 'too' and 'to' with the number '2'. 
+// Even if the sound is found mid word (like in octopus) or not in lowercase 
+// grandma still thinks that should be replaced with a 2. bless her.
+
+// 'I love to text' becomes 'I love 2 text' 'see you tomorrow' becomes 'see you 
+// 2morrow' 'look at that octopus' becomes 'look at that oc2pus'
+
+// Note that 'too' should become '2', not '2o'
+
+function textin(str){
+  return str.replace(/two|too|to/gi, '2')
+}
