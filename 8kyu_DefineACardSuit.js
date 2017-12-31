dae2a7c1836b727df9e1f1e6544fda6de4ee3978
Tong Lin <brooklyntechlin@gmail.com>
2017-12-31 00:17:03 -0500

    added 8kyu - Define a Card Suit

---
+++ 8kyu_DefineACardSuit.js
@@ -0,0 +1,33 @@
+// 8kyu - Define a Card Suit
+
+// You get any card as an argument. Your task is to return a suit of this card.
+
+// Our deck (is preloaded):
+
+// deck = ['2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣','A♣',
+//         '2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦','A♦',
+//         '2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥','A♥',
+//         '2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠','A♠'];
+// ('3♣') -> return 'clubs'
+// ('3♦') -> return 'diamonds'
+// ('3♥') -> return 'hearts'
+// ('3♠') -> return 'spades'
+
+function defineSuit(card) {
+  let temp = "";
+  switch(card.replace(/\w/gi, "")){
+    case("♣"):
+      temp = "clubs";
+      break;
+    case("♦"):
+      temp = "diamonds";
+      break;
+    case("♥"):
+      temp = "hearts";
+      break;
+    case("♠"):
+      temp = "spades";
+      break;
+  }
+  return temp;
+}
