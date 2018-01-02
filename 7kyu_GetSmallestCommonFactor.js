9770ff23f3923310c5e3d1829fe25bec883e3577
Tong Lin <brooklyntechlin@gmail.com>
2018-01-02 00:10:44 -0500

    added 7kyu - Get Smallest Common Factor

---
+++ 7kyu_GetSmallestCommonFactor.js
@@ -0,0 +1,22 @@
+// 7kyu - Get Smallest Common Factor
+
+// Given an array of integers, return the smallest common factors of all 
+
+// integers in the array.
+
+// When i say Smallest Common Factor i mean the smallest number above 1 that can 
+
+// divide all numbers in the array without a remainder.
+
+// scf([200, 30, 18, 8, 64, 34]) //=> 2
+// scf([21, 45, 51, 27, 33]) //=> 3
+// scf([133, 147, 427, 266]) //=> 7
+// If ther'e no common factors above 1, return 1 (technically 1 is always 
+// a common factor)
+
+
+let scf = array => {
+    for(let i = 2; i <= Math.min(...array); i++) 
+    if (array.every(n => n % i == 0)) 
+      return i;
+}
