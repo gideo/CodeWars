0772d72d875524be6bc60a5ab6266052a1c4a665
Tong Lin <brooklyntechlin@gmail.com>
2018-01-06 00:04:49 -0500

    added 8kyu - Returning String

---
+++ 8kyu_ReturningStrings.py
@@ -0,0 +1,9 @@
+# 8kyu - Returning Strings
+
+# Make a function that will return a greeting statement that uses an input; 
+# your program should return, "Hello, <name> how are you doing today?".
+
+# [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+def greet(name):
+    return "Hello, " + name + " how are you doing today?"
