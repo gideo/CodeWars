3c5eb3b3e9ba1aef7885d18963785e67d213c831
Tong Lin <brooklyntechlin@gmail.com>
2018-01-08 00:05:46 -0500

    added 8kyu - Aspect Ratio Cropping Part 1

---
+++ 8kyu_AspectRatioCropping_Part1.js
@@ -0,0 +1,16 @@
+// 8kyu - Aspect Ratio Cropping - Part 1
+
+// The aspect ratio of an image describes the proportional relationship between 
+// its width and its height. Most video shown on the internet uses a 16:9 aspect 
+// ratio, which means that for every pixel in the Y, there are roughly 1.77 
+// pixels in the X (where 1.77 ~= 16/9). As an example, 1080p video with an aspect 
+// ratio of 16:9 would have an X resolution of 1920, however 1080p video with an 
+// aspect ratio of 4:3 would have an X resolution of 1440.
+
+// Write a function that accepts arbitrary X and Y resolutions and converts them 
+// into resolutions with a 16:9 aspect ratio that maintain equal height. Round 
+// your answers up to the nearest integer.
+
+// This kata is part of a series with Aspect Ratio Cropping - Part 2.
+
+let aspectRatio = (x,y) => [Math.ceil(16*y/9), y];
