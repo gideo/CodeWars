62d770fecaa2f1ddbd9c2b9ab859efa73f77bfc9
Tong Lin <brooklyntechlin@gmail.com>
2018-01-15 00:07:47 -0500

    added 8kyu - Sort My Textbooks

---
+++ 8kyu_SortMyTextbooks.py
@@ -0,0 +1,10 @@
+# 8kyu - Sort My Textbooks
+
+# HELP! Jason can't find his textbook! It is two days before the test date, 
+# and Jason's textbooks are all out of order! Help him sort a list 
+# (ArrayList in java) full of textbooks by subject, so he can study before the test.
+
+# The sorting should NOT be case sensitive
+
+def sorter(textbooks):
+    return sorted(textbooks,key=str.lower)
