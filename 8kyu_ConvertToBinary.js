59a55977966f3e8d8501b5376e503e40c7806d77
Tong Lin <brooklyntechlin@gmail.com>
2018-01-18 00:01:20 -0500

    added 8kyu - Convert to Binary

---
+++ 8kyu_ConvertToBinary.js
@@ -0,0 +1,7 @@
+// 8kyu - Convert to Binary
+
+// Task Overview
+// Given a non-negative integer n, write a function toBinary/ToBinary 
+// which returns that number in a binary format.
+
+let toBinary = n => +(n>>>0).toString(2);
