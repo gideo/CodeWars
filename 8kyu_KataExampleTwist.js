75ca8063671befbfe11afed17a3d1b9828672480
Tong Lin <brooklyntechlin@gmail.com>
2018-01-27 00:04:27 -0500

    added 8kyu - Kata Example Twist

---
+++ 8kyu_KataExampleTwist.js
@@ -0,0 +1,10 @@
+// 8kyu - Kata Example Twist
+
+// This is an easy twist to the example kata (provided by Codewars when 
+// learning how to create your own kata).
+
+// Add the value "codewars" to the array websites/Websites 1,000 times.
+
+// var websites = []
+
+var websites = Array(1000).fill("codewars");
