24d616f5dd8d9ea3e7b746d0d2d5a1c574a2da98
Tong Lin <brooklyntechlin@gmail.com>
2018-01-28 00:13:52 -0500

    added 8kyu - Do you Speak English

---
+++ 8kyu_DoYouSpeakEnglish.js
@@ -0,0 +1,17 @@
+// 8kyu - Do you speak "English"
+
+// Given a string of arbitrary length with any ascii characters. 
+// Write a function to determine whether the string contains the 
+// whole word "English".
+
+// The order of characters is important -- a string "abcEnglishdef" is correct 
+// but "abcnEglishsef" is not correct.
+
+// Upper or lower case letter does not matter -- "eNglisH" is also correct.
+
+// Return value as boolean values, true for the string to contains "English", 
+// false for it does not.
+
+function spEng(sentence){
+  return /(english)/gi.test(sentence);
+}
