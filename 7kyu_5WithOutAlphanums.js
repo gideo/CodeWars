3beccdd50674824dabe5cb63d3a47efdcf599a6c
Tong Lin <brooklyntechlin@gmail.com>
2018-02-04 00:07:00 -0500

    added 7kyu - 5 Without Alphanums

---
+++ 7kyu_5WithOutAlphanums.js
@@ -0,0 +1,13 @@
+// 7kyu - 5 Without Alphanums!!!
+
+// Write a function/proc/lambda called _ that always returns 5.
+
+// Sounds easy right? Just bear in mind that you can only use the 
+// following characters:
+
+// ~!@#$%^&*()-_+=[]\{}|:;"'<,>.?/`
+// Kata inspired by 5 without numbers !!.
+
+// Good luck :)
+
+_=_=>-~-~-~-~-~[]
