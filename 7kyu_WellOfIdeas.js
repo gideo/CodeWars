6ee2cf35b02e101de170588cd6fca1a3b56852fe
Tong Lin <brooklyntechlin@gmail.com>
2018-02-07 00:10:17 -0500

    added 7kyu - Well of Ideas

---
+++ 7kyu_WellOfIdeas.js
@@ -0,0 +1,19 @@
+// 7kyu - Well of Ideas - Harder Version
+
+// For every good kata idea there seem to be quite a few bad ones!
+
+// In this kata you need to check the provided 2 dimensional array (x) for good 
+// ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 
+// 'Publish!', if there are more than 2 return 'I smell a series!'. If there are 
+// no good ideas, as is often the case, return 'Fail!'.
+
+// The sub arrays may not be the same length.
+
+// The solution should be case insensitive (ie good, GOOD and gOOd all count as 
+// a good idea). All inputs may not be strings.
+
+function well([arr, ...all]){
+  arr = arr.concat(...all);
+  let good = (arr.filter(a => typeof a === "string" ? a.toLowerCase() === "good" : false)).length;
+  return (good > 2) ? "I smell a series!" : (good > 0) ? "Publish!" : "Fail!";
+}
