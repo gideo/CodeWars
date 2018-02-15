b20befbaa5eebcde095d0fa513e089cca72dd562
Tong Lin <brooklyntechlin@gmail.com>
2018-02-15 00:04:44 -0500

    added 8kyu - Find Nearest Square Number

---
+++ 8kyu_FindNearestSquareNumber.js
@@ -0,0 +1,9 @@
+// 8kyu - Find Nearest Square Number
+
+// Your task is to find the nearest square number, nearest_sq(n), of a 
+// positive number n.
+
+// Goodluck :)
+
+def nearest_sq(n):
+    return round(n**0.5)**2
