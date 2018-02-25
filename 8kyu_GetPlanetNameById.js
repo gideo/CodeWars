5eab2977044a091cccbbed78a1ead64819db12cf
Tong Lin <brooklyntechlin@gmail.com>
2018-02-25 00:26:32 -0500

    added 8kyu - Get Planet Name by ID

---
+++ 8kyu_GetPlanetNameById.js
@@ -0,0 +1,36 @@
+// 8kyu - Get Planet Name By ID
+
+// The function is not returning the correct values. Can you figure out why?
+
+// getPlanetName(3); // should return 'Earth'
+
+function getPlanetName(id){
+  var name;
+  switch(id){
+    case 1:
+      name = 'Mercury'
+      break;
+    case 2:
+      name = 'Venus'
+      break;
+    case 3:
+      name = 'Earth'
+      break;
+    case 4:
+      name = 'Mars'
+      break;
+    case 5:
+      name = 'Jupiter'
+      break;
+    case 6:
+      name = 'Saturn'
+      break;
+    case 7:
+      name = 'Uranus'
+      break;
+    case 8:
+      name = 'Neptune'
+  }
+  
+  return name;
+}
