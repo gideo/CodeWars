276413f2a384084e5b0a4ac76a53ca7234aad8e8
Tong Lin <brooklyntechlin@gmail.com>
2018-03-07 00:20:46 -0500

    added 8kyu - Fundamentals - Return

---
+++ 8kyu_FundamentalsReturn.js
@@ -0,0 +1,27 @@
+// 8kyu - Fundamentals:Return
+
+// Make multiple functions that will return the sum, difference, modulus, 
+// product, quotient, and the exponent respectively.
+
+// Please use the following function names:
+
+// addition = add
+
+// multiply = multiply
+
+// division = divide (both integer and float divisions are accepted)
+
+// modulus = mod
+
+// exponential = exponent
+
+// subtraction = subt
+
+// Note: All math operations will be: a (operation) b
+
+add = lambda a, b: a + b
+subt = lambda a, b:a - b
+multiply = lambda a, b: a * b
+divide = lambda a, b: a / b
+mod = lambda a, b: a % b
+exponent = lambda a, b: a ** b
