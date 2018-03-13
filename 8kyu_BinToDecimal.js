bcbb60bd5bd18fb3d9f773553a6ae66c3a5f2ea2
Tong Lin <brooklyntechlin@gmail.com>
2018-03-13 00:10:09 -0400

    added 8kyu - Bin to Decimal

---
+++ 8kyu_BinToDecimal.js
@@ -0,0 +1,8 @@
+// 8kyu - Bin to Decimal
+
+// Complete the function which converts a binary number (given as a string) 
+// to a decimal number.
+
+function binToDec(bin){
+  return parseInt(bin, 2);
+}
