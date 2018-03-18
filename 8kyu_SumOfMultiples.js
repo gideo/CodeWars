dd5d08aa49757f822c561c223564487c003dc7d8
Tong Lin <brooklyntechlin@gmail.com>
2018-03-18 00:07:11 -0400

    added 8kyu - Sum of Multiples

---
+++ 8kyu_SumOfMultiples.js
@@ -0,0 +1,21 @@
+// 8kyu - Sum of Multiples
+
+// Your Job
+// Find the sum of all multiples of n below m
+
+// Keep in Mind
+// n and m are natural numbers (positive integers)
+// m is excluded from the multiples
+// Examples
+// sumMul(2, 9)   ==> 2 + 4 + 6 + 8 = 20
+// sumMul(3, 13)  ==> 3 + 6 + 9 + 12 = 30
+// sumMul(4, 123) ==> 4 + 8 + 12 + ... = 1860
+// sumMul(4, -7)  ==> "INVALID"
+
+function sumMul(n,m){
+  var sum = 0;
+  for (let i = n; i < m; i+=n)
+    sum += i;
+    
+  return (n >= m) ? "INVALID" : sum;
+}
