66b41dcb4ec626e1f5ef0ae092ebb719a302b0a2
Tong Lin <brooklyntechlin@gmail.com>
2018-03-23 00:05:19 -0400

    added 7kyu - Binary to String

---
+++ 7kyu_BinaryToString.js
@@ -0,0 +1,14 @@
+// 7kyu - Binary to String
+
+// Your computer has forgotten how to speak ASCII! (or Unicode, whatever) It can 
+// only communicate in binary, and it has something important to tell you. Write a 
+// function which will receive a long string of binary code and convert it to a 
+// string. Remember, in Python binary output starts with '0b'.
+
+// As an example: binary_to_string('0b10000110b11000010b1110100') == 'Cat'
+
+// Input may consist of upper and lower case letters and numbers, in binary form of 
+// course, as well as special symbols. The input to your function will always be 
+// one string of binary.
+
+let binaryToString = b => b.split('0b').filter(x=>x!='').map(e=>String.fromCharCode(parseInt(e,2))).join``
