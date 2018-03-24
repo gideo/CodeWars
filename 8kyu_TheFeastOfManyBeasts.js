0cd4b5ea587a7267b6121b1a47a04458cc2f8db7
Tong Lin <brooklyntechlin@gmail.com>
2018-03-24 00:07:22 -0400

    added 8kyu - The Feast of Many Beasts

---
+++ 8kyu_TheFeastOfManyBeasts.js
@@ -0,0 +1,17 @@
+// 8kyu - The Feast of Many Beasts
+
+// All of the animals are having a feast! Each animal is bringing one dish. 
+// There is just one rule: the dish must start and end with the same letters as 
+// the animal's name. For example, the great blue heron is bringing garlic naan 
+// and the chickadee is bringing chocolate cake.
+
+// Write a function that takes the animal's name and dish as arguments and 
+// returns true or false to indicate whether the beast is allowed to bring the 
+// dish to the feast.
+
+// Assume that beast and dish are always lowercase strings, and that each has 
+// at least two letters. beast and dish may contain hyphens and spaces, but 
+// these will not appear at the beginning or end of the string. They will not 
+// contain numerals.
+
+let feast = (b,d) =>  (b[0] === d[0] && b[b.length-1] === d[d.length-1]);
