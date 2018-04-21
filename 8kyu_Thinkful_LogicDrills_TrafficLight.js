6a04bd80a4f60c080872b32977635975bc902cc0
Tong Lin <brooklyntechlin@gmail.com>
2018-04-21 00:06:55 -0400

    added 8kyu - Thinkful - Logical Drills - Traffic Light

---
+++ 8kyu_Thinkful_LogicDrills_TrafficLight.js
@@ -0,0 +1,13 @@
+// 8kyu - Thinkful - Logical Drills: Traffic Light
+
+// You're writing code to control your town's traffic lights. 
+// You need a function to handle each change from green, to yellow, to red, 
+// and then to green again.
+
+// Complete the function that takes a string as an argument representing the 
+// current state of the light and returns a string representing the state the 
+// light should change to.
+
+// For example, update_light('green') should return 'yellow'.
+
+let updateLight=c=> c === 'yellow' ? 'red' : c === 'green' ? 'yellow' : 'green'
