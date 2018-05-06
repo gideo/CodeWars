71e2c09c9658899ca30dc4da60287a6eb078e0c6
Tong Lin <brooklyntechlin@gmail.com>
2018-05-06 00:05:13 -0400

    added 7kyu - Roung the Next 5

---
+++ 7kyu_RoundTheNext5.js
@@ -0,0 +1,23 @@
+// 7kyu - Round to the Next 5
+
+// Given an integer as input, can you round it to the next (meaning, "higher") 5?
+
+// Examples:
+
+// input:    output:
+// 0    ->   0
+// 2    ->   5
+// 3    ->   5
+// 12   ->   15
+// 21   ->   25
+// 30   ->   30
+// -2   ->   0
+// -5   ->   -5
+// etc.
+// Input may be any positive or negative integer (including 0).
+
+// You can assume that all inputs are valid integers.
+
+function roundToNext5(n){
+  return Math.ceil(n/5)*5;
+}
