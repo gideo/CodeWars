9b35e1b52676a4c22929101286d7473eb1168998
Tong Lin <brooklyntechlin@gmail.com>
2018-05-08 00:06:16 -0400

    added 7kyu - How long should cook this for?

---
+++ 7kyu_HowLongShouldYouCookThisFor.js
@@ -0,0 +1,40 @@
+// 7kyu - How long should you cook this for?
+
+// You've purchased a ready-meal from the supermarket.
+
+// The packaging says that you should microwave it for 4 minutes and 20 seconds, 
+// based on a 600W microwave.
+
+// Oh no, your microwave is 800W! How long should you cook this for?!
+
+// Input
+// You'll be given 4 arguments:
+
+// 1. needed power
+// The power of the needed microwave.
+// Example: "600W"
+
+// 2. minutes
+// The number of minutes shown on the package.
+// Example: 4
+
+// 3. seconds
+// The number of seconds shown on the package.
+// Example: 20
+
+// 4. power
+// The power of your microwave.
+// Example: "800W"
+
+// Output
+// The amount of time you should cook the meal for formatted as a string.
+// Example: "3 minutes 15 seconds"
+
+// Note: the result should be rounded up.
+
+// 59.2 sec  -->  60 sec  -->  return "1 minute 0 seconds"
+
+function cookingTime(n, m, s, p) {
+  let time = Math.ceil(parseInt(n) / parseInt(p)* (60 * m + s));
+  return `${Math.floor(time / 60)} minutes ${time % 60} seconds`;
+}
