b4406b5b44a8744cb715ba967b84490cd96fe548
Tong Lin <brooklyntechlin@gmail.com>
2018-05-12 00:05:09 -0400

    added 8kyu - Sort and Star

---
+++ 8kyu_SortAndStar.js
@@ -0,0 +1,10 @@
+// 8kyu - Sort and STar
+
+// You will be given an vector of string(s). You must sort it alphabetically 
+// (case-sensitive!!) and then return the first value.
+
+// The returned value must be a string, and have "***" between each of its letters.
+
+// You should not remove or add elements from/to the array.
+
+let twoSort = s => s.sort()[0].split('').join('***');
