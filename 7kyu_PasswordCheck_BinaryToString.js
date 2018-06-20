a06d03ed9935c2a1463d128c5df46094d8d4ce4e
Tong Lin <brooklyntechlin@gmail.com>
2018-06-20 00:06:16 -0400

    added 7kyu - Password Check - Binary to String

---
+++ 7kyu_PasswordCheck_BinaryToString.js
@@ -0,0 +1,27 @@
+// 7kyu - Password Check - Binar to String
+
+// Password Check - Binary to String
+// A wealthy client has forgotten the password to his business website, but 
+// he has a list of possible passwords. His previous developer has left a file 
+// on the server with the name password.txt. You open the file and realize 
+// it's in binary format.
+
+// Write a script that takes an array of possible passwords and a string of 
+// binary representing the possible password. Convert the binary to a string 
+// and compare to the password array. If the password is found, return the 
+// password string, else return false;
+
+// decodePass(['password123', 'admin', 'admin1'], '01110000 01100001 01110011 
+// 	01110011 01110111 01101111 01110010 01100100 00110001 00110010 00110011');    
+// 	=> 'password123'
+// decodePass(['password321', 'admin', 'admin1'], '01110000 01100001 01110011 
+// 	01110011 01110111 01101111 01110010 01100100 00110001 00110010 00110011');    
+// 	=> false
+// decodePass(['password456', 'pass1', 'test12'], '01110000 01100001 01110011 
+// 	01110011 01110111 01101111 01110010 01100100 00110001 00110010 00110011');    
+// 	=> false
+
+function decodePass( passArr, bin ){
+  let pass = bin.split(' ').reduce( (str,bin)=>str +  String.fromCharCode(parseInt(bin,2)),"");
+  return passArr.includes(pass) ? pass : false;
+}
