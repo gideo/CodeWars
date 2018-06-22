048ef8ced0b800ccf9568f3e88d581c97a41d73f
Tong Lin <brooklyntechlin@gmail.com>
2018-06-22 00:01:58 -0400

    added 8kyu - Is it a number?

---
+++ 8kyu_IsItANumber.js
@@ -0,0 +1,19 @@
+// 8kyu - Is it a number?
+
+// Given a string s, write a method (function) that will return true if its 
+// a valid single integer or floating number or false if its not.
+
+// Valid examples, should return true:
+
+// isDigit("3")
+// isDigit("  3  ")
+// isDigit("-3.23")
+// should return false:
+
+// isDigit("3-4")
+// isDigit("  3   5")
+// isDigit("3 5")
+// isDigit("zero")
+
+
+let isDigit = s => s == parseFloat(s);
