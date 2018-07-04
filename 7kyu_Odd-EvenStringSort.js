199ec8468a3c8da747ea83dcfdb1c9e363609b67
Tong Lin <brooklyntechlin@gmail.com>
2018-07-04 06:41:36 -0400

    added 7kyu - Odd-Even String Sort

---
+++ 7kyu_Odd-EvenStringSort.js
@@ -0,0 +1,27 @@
+// 7kyu - Odd-Even String Sort
+
+// Given a string S. You have to return another string such that 
+// even-indexed and odd-indexed characters of S are grouped and groups 
+// are space-separated (see sample below)
+
+// Note: 
+// 0 is considered to be an even index. 
+// All input strings are valid with no spaces
+// input: 'CodeWars' output 'CdWr oeas'
+// S[0] = 'C'
+// S[1] = 'o'
+// S[2] = 'd'
+// S[3] = 'e'
+// S[4] = 'W'
+// S[5] = 'a'
+// S[6] = 'r'
+// S[7] = 's'
+// Even indices 0, 2, 4, 6, so we have 'CdWr' as the first group
+// odd ones are 1, 3, 5, 7, so the second group is 'oeas'
+// And the final string to return is 'Cdwr oeas'
+
+function sortMyString(S) {
+  let even = S.split('').filter((_,i) => i % 2).join(''),
+      odd = S.split('').filter((_,i) => !(i % 2)).join('');
+  return `${odd} ${even}`;
+}
