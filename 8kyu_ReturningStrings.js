8f17e9316493bed90759d78b5f542195745c72a0
Tong Lin <brooklyntechlin@gmail.com>
2018-07-21 05:48:13 -0400

    added 8kyu - Returning Strings

---
+++ 8kyu_ReturningStrings.js
@@ -0,0 +1,11 @@
+// 8kyu - Returning String
+
+// Make a function that will return a greeting statement that uses an input; 
+// your program should return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute 
+// properly]
+
+function greet(name){
+  return `Hello, ${name} how are you doing today?`;
+}
