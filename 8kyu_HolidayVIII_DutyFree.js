e504ac6bcf5abb6926df43b588e6b2dd928ab83f
Tong Lin <brooklyntechlin@gmail.com>
2018-08-06 06:56:18 -0400

    added 8kyu - Holiday VIII - Duty Free

---
+++ 8kyu_HolidayVIII_DutyFree.js
@@ -0,0 +1,16 @@
+// 8kyu - Holiday VIII - Duty Free
+
+// The purpose of this kata is to work out just how many bottles of 
+// duty free whiskey you would have to buy such that the saving over 
+// the normal high street price would effectively cover the cost of your 
+// holiday.
+
+// You will be given the high street price (normPrice), the duty free 
+// discount (discount) and the cost of the holiday.
+
+// For example, if a bottle cost £10 normally and the discount in duty 
+// free was 10%, you would save £1 per bottle. If your holiday cost £500, 
+// the answer you should return would be 500.
+
+All inputs will be integers. Please return an integer. Round down.
+let dutyFree = (n,d,h) => (Math.floor(h / n / d * 100));
