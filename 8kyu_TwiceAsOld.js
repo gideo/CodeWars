2c5acb1bfc16223efcf23222634de3478b31b871
Tong Lin <brooklyntechlin@gmail.com>
2018-08-31 07:15:04 -0400

    added 8kyu - Twice as old

---
+++ 8kyu_TwiceAsOld.js
@@ -0,0 +1,10 @@
+// 8kyu - Twice as old
+
+// Your function takes two arguments:
+
+// current father's age (years)
+// current age of his son (years)
+// Сalculate how many years ago the father was twice as old as his son 
+// (or in how many years he will be twice as old).
+
+let twiceAsOld =(a,b)=> Math.abs(a - 2 * b);
