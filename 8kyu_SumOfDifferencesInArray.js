c74b017a8f2bd37fbe182990f1364fda41058b72
Tong Lin <brooklyntechlin@gmail.com>
2018-09-02 05:23:11 -0400

    added 8kyu - Sum of Differences in Array

---
+++ 8kyu_SumOfDifferencesInArray.js
@@ -0,0 +1,15 @@
+// 8kyu - Sum of Differences in Array
+
+// Your task is to sum the differences between consecutive pairs in 
+// the array in descending order.
+
+// For example: sumOfDifferences([1, 2, 10] Returns 9
+
+// Descending order: [10, 2, 1]
+
+// Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
+
+// If the array is empty or the array has only one element the result 
+// should be 0.
+
+let sumOfDifferences = arr => (arr.length > 1) ? Math.max(...arr) - Math.min(...arr) : 0;
