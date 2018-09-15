b469832e0fe82ab9f1239156faa30b0b1c799c82
Tong Lin <brooklyntechlin@gmail.com>
2018-09-15 06:36:20 -0400

    added 8kyu - Enumrable Magic #25 - Take the First N Elements

---
+++ 8kyu_EnumerableMagic#25_TakeTheFirstNElements.js
@@ -0,0 +1,8 @@
+// 8kyu - Enumerable Magic #25 - Take the first N Elements
+
+// Create a method take that accepts a list/array and a number n, and 
+// returns a list/array array of the first n elements from the list/array.
+
+// If you need help, here's a reference:
+
+let take = (arr, n) => arr.slice(0, n);
