ed2eb395e13f48c3678c92998d679571f53192bb
Tong Lin <brooklyntechlin@gmail.com>
2018-10-17 06:05:45 -0400

    added 8kyu - Enumerable Magic #3 - Does My List Include This

---
+++ 8kyu_EnumerableMagic#3_DoesMyListIncludeThis.js
@@ -0,0 +1,11 @@
+// 8kyu - Enumerable Magic #3 - Does My List Include This?
+
+// Create a method include? (JS include)that accepts a list and an item, and returns true if the item belongs to the list.
+
+// Ruby: If you need help, here's some reference material:
+
+// http://www.rubycuts.com/enum-include
+
+function include(arr, item){
+  return arr.includes(item);
+}
