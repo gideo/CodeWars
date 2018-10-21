e9ea33936b7d8192a780e0d6376fcbc797d91ac5
Tong Lin <brooklyntechlin@gmail.com>
2018-10-21 07:23:36 -0400

    added 7kyu - Holiday III - Fire on the Boat

---
+++ 7kyu_HolidayIII_FireOnTheBoat.js
@@ -0,0 +1,11 @@
+// 7kyu - Holiday III - Fire on the Boat
+
+// Enjoying your holiday, you head out on a scuba diving trip!
+
+// Disaster!! The boat has caught fire!!
+
+// You will be provided a string that lists many boat related items. If any of these items are "Fire" you must spring into action. Change any instance of "Fire" into "~~". Then return the string.
+
+// Go to work!
+
+let fireFight = s => s.split(' ').map(a => a.replace("Fire", "~~")).join(" ");
