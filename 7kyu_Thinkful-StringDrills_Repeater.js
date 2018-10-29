130bbf8abbe84973ce6468cc4b245679b0c22446
Tong Lin <brooklyntechlin@gmail.com>
2018-10-29 07:32:26 -0400

    added 7kyu - Thinkful-String Drills - Repeater

---
+++ 7kyu_Thinkful-StringDrills_Repeater.js
@@ -0,0 +1,10 @@
+// 7kyu - Thinkful - String Drills: Repeater
+
+// Write a class function named repeat() that takes two arguments (a string and a long integer), and returns a new string where the input string is repeated that many times. For example:
+
+// Repeater.repeat("a", 5)
+// should return
+
+// "aaaaa"
+
+const repeater = (string, n) => string.repeat(n);
