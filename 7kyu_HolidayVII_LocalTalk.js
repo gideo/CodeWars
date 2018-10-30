887c491a3f000ef4b276b60d53626e0d83775d53
Tong Lin <brooklyntechlin@gmail.com>
2018-10-30 05:08:53 -0400

    added 7kyu - Holiday VII - Loca Talk

---
+++ 7kyu_HolidayVII_LocalTalk.js
@@ -0,0 +1,9 @@
+// 7kyu - Holiday VII - Local Talk
+
+// In Bali, as far as I can gather, when ex-pats speak to locals, they basically insert the word 'Pak' as often as possible. I am assured it means something like 'mate' or 'sir' but that could be completely wrong.
+
+// Anyway, as some basic language education(??) this kata requires you to turn any sentence provided (s) into ex-pat balinese waffle by inserting the word 'pak' between every other word. Simple 8kyu :D
+
+// Pak should not be the first or last word. Strings of just spaces should return an empty string.
+
+let pak = s => s.replace(/\s/g, " pak ");
