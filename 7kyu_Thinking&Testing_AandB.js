098b16020e91ec62ada7063846b13f0a9ff85274
Tong Lin <brooklyntechlin@gmail.com>
2018-11-03 06:25:00 -0400

    added 7kyu - Thinking & Testing - A and B

---
+++ 7kyu_Thinking&Testing_AandB.js
@@ -0,0 +1,10 @@
+// 7kyu - Thinking & Testing: A and B?
+// No Story
+
+// No Description
+
+// Only by Thinking and Testing
+
+// Look at result of testcase, guess the code!
+
+let testit = (a,b) => a | b;
