f3f5583d8fd8e83d95a68a52f4ca4dba94847b89
Tong Lin <brooklyntechlin@gmail.com>
2018-11-25 08:24:47 -0500

    added 8kyu - Volume of a Cuboid

---
+++ 8kyu_VolumeOfACuboid.js
@@ -0,0 +1,15 @@
+// 8kyu - Volume of a Cuboid
+// Bob needs a fast way to calculate the volume of a cuboid with three values: length, width and the height of the cuboid. Write a function to help Bob with this calculation.
+
+// Ruby: def get_volume_of_cuboid(length, width, height) Bash: bash run_shell length width height
+
+var Kata;
+
+Kata = (function() {
+  function Kata() {}
+
+  Kata.getVolumeOfCuboid = function(length, width, height){return length * width * height};
+
+  return Kata;
+
+})();
