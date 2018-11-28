5f6f453a4874baffc797df0c6b707fc1413b4426
Tong Lin <brooklyntechlin@gmail.com>
2018-11-28 07:41:31 -0500

    added 8kyu - Grasshopper Messi Goals Function

---
+++ 8kyu_Grasshopper_MessiGoalsFunction.js
@@ -0,0 +1,17 @@
+// 8kyu - Grasshopper - Messi Goals Function
+
+// Messi goals function
+// Messi is a soccer player with goals in three leagues:
+
+// LaLiga
+// Copa del Rey
+// Champions.
+// Complete the goals function to return his total goals (the sum) for all three leagues.
+
+// Note: the parameter for this function will always be a valid number.
+
+// Ex :
+
+// goals(5,10,2) == 5+10+2 = 17
+
+let goals = (a,b,c) => a + b + c;
