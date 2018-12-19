d18e85035bed8c3b95e5427a35bc1f8dfa25244e
Tong Lin <brooklyntechlin@gmail.com>
2018-12-19 07:19:27 -0500

    added 7kyu - Palindromic Expression

---
+++ 7kyu_PalindromicExpression.js
@@ -0,0 +1,19 @@
+// 7kyu - Palindromic Expression
+
+// Welcome to yet another kata on palindromes!
+
+// Your job is to return a string.
+
+// The first half of the string should contain valid code in your language.
+
+// When we receive your string, we will sum up the char codes of each character.
+
+// The first half of your string will hopefully represent a function expression, because we
+
+// will attempt to call it, passing in the sum as the input.
+
+// If the output of the function is the same as the sum, then you pass the kata.
+
+// The string must also be a palindrome in order for you to pass.
+
+let palindromicExpression = () => '()=>08100180>=)(';
