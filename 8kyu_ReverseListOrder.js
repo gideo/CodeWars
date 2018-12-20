26db87e2faf6a2f60c2373533ff40c3223ae4da1
Tong Lin <brooklyntechlin@gmail.com>
2018-12-20 07:36:46 -0500

    added8kyu - Reverse List Order

---
+++ 8kyu_ReverseListOrder.js
@@ -0,0 +1,9 @@
+// 8kyu - Reverse List Order
+
+// In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+// Examples
+// reverseList([1,2,3,4]) == [4,3,2,1]
+// reverseList([3,1,5,4]) == [4,5,1,3]
+
+let reverseList = list => list.reverse();
