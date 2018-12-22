13e0e0438f795ea6d73b2c447314745b4951be6b
Tong Lin <brooklyntechlin@gmail.com>
2018-12-22 07:55:28 -0500

    added 7kyu - Ski Jump

---
+++ 7kyu_SkiJump.js
@@ -0,0 +1,35 @@
+// 7kyu - Ski Jump
+
+// You are a skier (marked below by the 'X'). You have made it to the Olympics! Well done.
+
+// ___X_
+// *****\ 
+// ******\
+// *******\
+// ********\
+// *********\.____/
+// Your job in this kata is to calculate the maximum speed you will achieve during your downhill run. The speed is dictated by the height of the mountain. Each element of the array is a layer of the mountain as indicated by the diagram above (and further below). So for this example the mountain has a value of 5 (5 rows of stars). Speed is mountain height * 1.5.
+
+// The jump length is calculated by (mountain height * speed * 9) / 10. Jump length should be to two dp.
+
+// You must return the length of the resulting jump as a string in the following format:
+
+// Jump < 10 = 'X metres: He's crap!'
+// Jump > 10 && < 25 = 'X metres: He's ok!'
+// Jump > 10 && < 50 = 'X metres: He's flying!'
+// Jump > 50 = 'X metres: Gold!!'
+
+// In this case the right answer would be '33.75 metres: He\'s flying!'
+
+// Sadly it takes a lot of time to make arrays look like mountains, so the tests wont all look so nice. To give an example, the above mountain would look as follows in most cases:
+
+// [*****, ******, *******, ********, *********]
+// Not as much fun, eh?
+
+function skiJump(mountain){
+  let metres = ((Math.pow(mountain.length, 2) *1.5 * 9) / 10).toFixed(2),
+      str = (metres < 10) ? "He's crap!" : 
+            (metres < 25) ? "He's ok!" : 
+            (metres < 50) ? "He's flying!" : "Gold!!";
+  return `${metres} metres: ${str}`
+}
