a28f5cf009a62b916fd12c6172e93dcd584fd0da
Tong Lin <brooklyntechlin@gmail.com>
2018-12-26 07:39:23 -0500

    added 7kyu - Recursive Replication

---
+++ 7kyu_RecursiveReplication.js
@@ -0,0 +1,11 @@
+// 7kyu - Recusive Replication
+
+// You need to design a recursive function called replicate which will receive arguments times and number.
+
+// The function should return an array containing repetitions of the number argument. For instance, replicate(3, 5) should return [5,5,5]. If the times argument is negative, return an empty array.
+
+// As tempting as it may seem, do not use loops to solve this problem.
+
+function replicate(times, number) {
+  return (times > 0) ? Array(times).fill(number) : [];
+}
