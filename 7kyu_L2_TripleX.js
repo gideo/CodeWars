a43221deea1a6bb4892785a64583171586ea6ed4
Tong Lin <brooklyntechlin@gmail.com>
2019-01-09 07:04:47 -0500

    added 7kyu - L2 Triple X

---
+++ 7kyu_L2_TripleX.js
@@ -0,0 +1,15 @@
+// 7kyu - L2: Triple X
+
+// Given a string, return true if the first instance of "x" in the string is immediately followed by the string "xx".
+
+// tripleX("abraxxxas") → true
+// tripleX("xoxotrololololololoxxx") → false
+// tripleX("softX kitty, warm kitty, xxxxx") → true
+// tripleX("softx kitty, warm kitty, xxxxx") → false
+
+// Note :
+
+//     capital X's do not count as an occurrence of "x".
+//     if there are no "x"'s then return false
+
+let tripleX = str => /^[^x]*xxx/.test(str);
