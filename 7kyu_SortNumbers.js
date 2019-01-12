bc782f48901838aaea09a43dd0bd6c1de7241ca8
Tong Lin <brooklyntechlin@gmail.com>
2019-01-12 07:28:55 -0500

    added 7kyu - Sort Numbers

---
+++ 7kyu_SortNumbers.js
@@ -0,0 +1,12 @@
+// 7kyu - Sort Numbers
+
+// Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+// For example:
+
+// solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
+// solution(null); // should return []
+
+function solution(nums){
+  return (nums || []).sort(function(a, b){return a - b});
+}
