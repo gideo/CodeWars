0181964870c0c325fec7313e8a09ec0044b2dbf2
Tong Lin <brooklyntechlin@gmail.com>
2019-02-10 07:21:50 -0500

    added 7kyu - Frequency Sequence

---
+++ 7kyu_FrequencySequence.js
@@ -0,0 +1,9 @@
+// 7kyu - Frequency Sequence
+
+// Return an output string that translates an input string s/$s by replacing each character in s/$s with a number representing the number of times that character occurs in s/$s and separating each number with the character(s) sep/$sep.
+
+// freq_seq("hello world", "-"); // => "1-1-3-3-2-1-1-2-1-3-1"
+// freq_seq("19999999", ":"); // => "1:7:7:7:7:7:7:7"
+// freq_seq("^^^**$", "x"); // => "3x3x3x2x2x1"
+
+let freqSeq = (str, sep) => str.split('').map((v, i, arr) => arr.filter(vi => vi === v).length).join(sep);
