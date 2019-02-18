1ebfa4736b1230fbd1174f437472097740f4159a
Tong Lin <brooklyntechlin@gmail.com>
2019-02-18 07:16:39 -0500

    added 8kyu - Remove Exclamation Marks

---
+++ 8kyu_RemoveExclamationMarks.js
@@ -0,0 +1,5 @@
+// 8kyu - Remove Exclamation Marks
+
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+let removeExclamationMarks = s => s.replace(/!/gi, '');
