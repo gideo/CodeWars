d79d69a723730b70268be7220beae4752e5e49ad
Tong Lin <brooklyntechlin@gmail.com>
2019-02-21 07:27:46 -0500

    added 7kyu -Hex Word

---
+++ 7kyu_HexWordSum.py
@@ -0,0 +1,22 @@
+// 7kyu - Hex Word Sum
+
+// Description
+// As hex values can include letters A through to F, certain English words can be spelled out, such as CAFE, BEEF, or FACADE. This vocabulary can be extended by using numbers to represent other letters, such as 5EAF00D, or DEC0DE5.
+
+// Given a string, your task is to return the decimal sum of all words in the string that can be interpreted as such hex values.
+
+// Example
+// Working with the string BAG OF BEES:
+
+// BAG ==> 0 as it is not a valid hex value
+// OF ==> 0F ==> 15
+// BEES ==> BEE5 ==> 48869
+// So hex_word_sum('BAG OF BEES') returns the sum of these, 48884.
+
+// Notes
+// Inputs are all uppercase and contain no punctuation
+// 0 can be substituted for O
+// 5 can be substituted for S
+
+def hex_word_sum(s):
+  return sum(int(w, 16) for w in s.replace('O', '0').replace('S', '5').split() if all(a in '0123456789ABCDEF' for a in w))
