6aa427f23721e9d4b4652d6d89956b9770fea455
Tong Lin <brooklyntechlin@gmail.com>
2019-02-27 07:31:28 -0500

    added 7kyu - Two to One

---
+++ 7kyu_TwoToOne.js
@@ -0,0 +1,14 @@
+// 7kyu - Two to One
+
+// Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters,
+
+// each taken only once - coming from s1 or s2.
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+const longest = (s1, s2) => [...new Set(s1+s2)].sort().join('');
