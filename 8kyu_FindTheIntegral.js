db0f8a11f2d0500bfe6daab76483310a4fff92f1
Tong Lin <brooklyntechlin@gmail.com>
2019-03-01 07:22:15 -0500

    added 8kyu - Find the Integral

---
+++ 8kyu_FindTheIntegral.js
@@ -0,0 +1,15 @@
+// 8kyu - Find the Integral
+
+// Create a function that finds the integral of the expression passed.
+
+// In order to find the integral all you need to do is add one to the exponent (the second number), and divide the coefficient by that new number (the first number).
+
+// In 3x^2, for example, the integral would be 1x^3 (we added 1 to the exponent, and divided the coefficient by that new number).
+
+// integrate(3,2) // => "1x^3"
+// integrate(9,5) // => "1.5x^6"
+// Note that the output should be a string. The coefficient is always positive. Neither number will ever be 0.
+
+let integrate = (coefficient, exponent) => {
+  return coefficient / (exponent + 1) + 'x^' + (exponent + 1);
+}
