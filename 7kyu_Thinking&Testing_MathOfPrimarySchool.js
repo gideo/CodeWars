474f907a0b6d78fd7bdd8947169a7596e5658242
Tong Lin <brooklyntechlin@gmail.com>
2019-03-13 07:39:55 -0400

    added 7kyu - Thinking & Testing - Math of Primary School

---
+++ 7kyu_Thinking&Testing_MathOfPrimarySchool.js
@@ -0,0 +1,11 @@
+// 7kyu - Thinking & Testing: Math of Primary School
+
+// No Story
+
+// No Description
+
+// Only by Thinking and Testing
+
+// Look at result of testcase, guess the code!
+
+let testit = arr => arr[0] * arr[3] + arr[1] * arr[2];
