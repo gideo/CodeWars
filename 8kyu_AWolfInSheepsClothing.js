576b02cfccffa721ef351e84d2554de2012d0191
Tong Lin <brooklyntechlin@gmail.com>
2019-03-18 07:30:55 -0400

    added 8kyu - A Wolf in Sheeps Clothing

---
+++ 8kyu_AWolfInSheepsClothing.js
@@ -0,0 +1,22 @@
+// 8kyu - A Wolf in Sheep's Clothing 
+
+// A wolf in sheep's clothing
+// Wolves have been reintroduced to Great Britain. You are a sheep farmer, and are now plagued by wolves which pretend to be sheep. Fortunately, you are good at spotting them.
+
+// Warn the sheep in front of the wolf that it is about to be eaten. Remember that you are standing at the front of the queue which is at the end of the array:
+
+// [sheep, sheep, sheep, sheep, sheep, wolf, sheep, sheep]      (YOU ARE HERE AT THE FRONT OF THE QUEUE)
+//    7      6      5      4      3            2      1
+// If the wolf is the closest animal to you, return "Pls go away and stop eating my sheep". Otherwise, return "Oi! Sheep number N! You are about to be eaten by a wolf!" where N is the sheep's position in the queue.
+
+// Note: there will always be exactly one wolf in the array.
+
+// Examples
+// warnTheSheep(["sheep", "sheep", "sheep", "wolf", "sheep"]) === "Oi! Sheep number 1! You are about to be eaten by a wolf!"
+
+// warnTheSheep(["sheep", "sheep", "wolf"]) === "Pls go away and stop eating my sheep"
+
+function warnTheSheep(queue) {
+  queue = queue.reverse().indexOf('wolf');
+  return (queue === 0) ? 'Pls go away and stop eating my sheep' : `Oi! Sheep number ${ queue }! You are about to be eaten by a wolf!`;
+}
