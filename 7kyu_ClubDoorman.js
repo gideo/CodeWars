104b8d25d61faedd55cc9f1ccde5e89a1cf6791a
Tong Lin <brooklyntechlin@gmail.com>
2019-04-01 07:12:02 -0400

    added 7kyu - Club Doorman

---
+++ 7kyu_ClubDoorman.js
@@ -0,0 +1,26 @@
+// 7kyu - Club Doorman
+
+// INTRODUCTION
+// The Club Doorman will give you a word. To enter the Club you need to provide the right number according to provided the word.
+
+// Every given word has a doubled letter, like 'tt' in lettuce.
+
+// To answer the right number you need to find the doubled letter's position of the given word in the alphabet and multiply this number per 3.
+
+// It will be given only words with one doubled letter.
+
+
+// EXAMPLE
+// Lettuce is the given word. 't' is the doubled letter and it's position is 20 in the alphabet.
+
+// The answer to the Club Doorman is 20 * 3 = 60
+
+function passTheDoorMan(word) {
+  let str;
+  for (let i = 0; i < word.length-1; ++i)
+    if (word[i]==word[i+1]) {
+      str=word[i];
+      break;
+    }
+  return (str.charCodeAt()-96)*3;
+}
