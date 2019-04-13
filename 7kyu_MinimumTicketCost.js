fbc4412b6befc573ff17ae61ad86bf708aa4439e
Tong Lin <brooklyntechlin@gmail.com>
2019-04-13 07:49:56 -0400

    added 7kyu - Minimum Ticket Cost

---
+++ 7kyu_MinimumTicketCost.js
@@ -0,0 +1,29 @@
+// 7kyu - Minimum Ticket Cost 
+
+// This is a question from codingbat
+
+// Given an integer n greater than or equal to 0, create and return an array with the following pattern:
+
+// squareUp(3) => [0, 0, 1, 0, 2, 1, 3, 2, 1]
+// squareUp(2) => [0, 1, 2, 1]
+// squareUp(4) => [0, 0, 0, 1, 0, 0, 2, 1, 0, 3, 2, 1, 4, 3, 2, 1]
+// n<=1000.
+
+// Check out my other kata!
+// Matrix Diagonal Sort OMG
+
+// String -> N iterations -> String
+
+// String -> X iterations -> String
+
+// ANTISTRING
+
+// Array - squareUp b!
+
+// Matrix - squareUp b!
+
+// Infinitely Nested Radical Expressions
+
+// pipi Numbers!
+
+let findJane = n => Math.ceil(n/2)-1;
