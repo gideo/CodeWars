36f732122a01b1a36b4a3ee86859927f509c26e9
Tong Lin <brooklyntechlin@gmail.com>
2019-04-15 07:21:02 -0400

    added 7kyu - Thinking & Testing - Math of Middle School

---
+++ 7kyu_Thinking&Testing_MathOfMiddleSchool.js
@@ -0,0 +1,11 @@
+// 7kyu - Thinking & Testing : Math of Middle school
+
+// No Story
+
+// No Description
+
+// Only by Thinking and Testing
+
+// Look at result of testcase, guess the code!
+
+let testit = (a, b) => [ a[0] * b[0] + a[1] * b[2], a[0] * b[1] + a[1] * b[3], a[2] * b[0] + a[3] * b[2], a[2] * b[1] + a[3] * b[3]];
