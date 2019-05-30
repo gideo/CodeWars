138a560f101471932ebb0ede958777e9339d3f37
Tong Lin <brooklyntechlin@gmail.com>
2019-05-29 20:16:43 -0400

    added 7kyu - Odds-Index

---
+++ 7kyu_Odds-Index.js
@@ -0,0 +1,9 @@
+// 7kyu - Odds-Index
+
+// You are given an array with several "even" words, one "odd" word, and some numbers mixed in.
+
+// Determine if any of the numbers in the array is the index of the "odd" word. If so, return true, otherwise false.
+
+function oddBall(arr){
+    return arr.includes(arr.indexOf("odd"));
+}
