3d07017a6925833db50d8d1365bd0edcc4799740
Tong Lin <brooklyntechlin@gmail.com>
2019-06-01 07:09:15 -0400

    added 6kyu - Moves in Squared String (III)

---
+++ 6kyu_MovesInSquaredString(III).js
@@ -0,0 +1,55 @@
+// 6kyu - Moves in Squared Strings(III)
+
+// You are given a string of n lines, each substring being n characters long: For example:
+
+// s = "abcd\nefgh\nijkl\nmnop"
+
+// We will study some transformations of this square of strings.
+
+// Symmetry with respect to the main diagonal: diag_1_sym (or diag1Sym or diag-1-sym)
+// diag_1_sym(s) => "aeim\nbfjn\ncgko\ndhlp"
+// Clockwise rotation 90 degrees: rot_90_clock (or rot90Clock or rot-90-clock)
+// rot_90_clock(s) => "miea\nnjfb\nokgc\nplhd"
+// selfie_and_diag1(s) (or selfieAndDiag1 or selfie-and-diag1) It is initial string + string obtained by symmetry with respect to the main diagonal.
+// s = "abcd\nefgh\nijkl\nmnop" --> 
+// "abcd|aeim\nefgh|bfjn\nijkl|cgko\nmnop|dhlp"
+// or printed for the last:
+// selfie_and_diag1
+// abcd|aeim
+// efgh|bfjn
+// ijkl|cgko 
+// mnop|dhlp
+// #Task:
+
+// Write these functions diag_1_sym, rot_90_clock, selfie_and_diag1
+// and
+
+// high-order function oper(fct, s) where
+
+// fct is the function of one variable f to apply to the string s (fct will be one of diag_1_sym, rot_90_clock, selfie_and_diag1)
+// #Examples:
+
+// s = "abcd\nefgh\nijkl\nmnop"
+// oper(diag_1_sym, s) => "aeim\nbfjn\ncgko\ndhlp"
+// oper(rot_90_clock, s) => "miea\nnjfb\nokgc\nplhd"
+// oper(selfie_and_diag1, s) => "abcd|aeim\nefgh|bfjn\nijkl|cgko\nmnop|dhlp"
+// Notes:
+// The form of the parameter fct in oper changes according to the language. You can see each form according to the language in "Your test cases".
+// It could be easier to take these katas from number (I) to number (IV)
+// Bash Note: The ouput strings should be separated by \r instead of \n. See "Sample Tests".
+// A forthcoming kata will study other tranformations.
+
+function rot90Clock(s) {
+    return diag1Sym(s).map(v => [...v].reverse().join(''));
+}
+
+function diag1Sym(s) {
+    return s.map((v, i) => [...v].map((_, j) => s[j][i]).join(''));
+}
+function selfieAndDiag1(s) {
+    const r = diag1Sym(s)
+    return s.map((v, i) => v + '|' + r[i]);
+}
+function oper(fct, s) {
+    return fct(s.split('\n')).join('\n');
+}
