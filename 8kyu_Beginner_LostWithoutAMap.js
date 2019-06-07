4d653a1cc8115729582f8c0353a7fbee5aa5ca65
Tong Lin <brooklyntechlin@gmail.com>
2019-06-07 07:02:07 -0400

    added 8kyu - Beginner - Lost Without a Map

---
+++ 8kyu_Beginner_LostWithoutAMap.js
@@ -0,0 +1,13 @@
+// 8kyu - Beginner - Lost Without a Map
+
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+// For the beginner, try to use the map method - it comes in very handy quite a lot so is a good one to know.
+
+function maps(x){
+  return x.map(n => n * 2);
+}
