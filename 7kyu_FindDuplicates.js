23e58249d11aa304dd6407725a9f4e13812d629f
Tong Lin <brooklyntechlin@gmail.com>
2019-06-24 06:03:40 -0400

    added 7kyu - Duplicates

---
+++ 7kyu_FindDuplicates.js
@@ -0,0 +1,11 @@
+// 7kyu - Find Duplicates 
+
+// Given an array, find the duplicates in that array, and return a new array of those duplicates. The elements of the returned array should appear in the order when they first appeared as duplicates.
+
+// Note: numbers and their corresponding string representations should not be treated as duplicates (i.e., '1' !== 1).
+
+// Examples
+// [1, 2, 4, 4, 3, 3, 1, 5, 3, '5']  ==>  [4, 3, 1]
+// [0, 1, 2, 3, 4, 5]                ==>  []
+
+let duplicates = arr => arr.filter(function(a, b) {return arr.indexOf(a) != b && arr.lastIndexOf(a) == b;});
