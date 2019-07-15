ee82b8a8f5eaeab714ea07da1b9f676adfe222b5
Tong Lin <brooklyntechlin@gmail.com>
2019-07-15 06:31:42 -0400

    added 7kyu - Simple Fun #152 - Invite More Women

---
+++ 7kyu_SimpleFun#152_InviteMoreWomen.js
@@ -0,0 +1,21 @@
+// 7kyu - Simple Fun #152 - Invite More Women
+
+// Task
+// King Arthur and his knights are having a New Years party. Last year Lancelot was jealous of Arthur, because Arthur had a date and Lancelot did not, and they started a duel.
+
+// To prevent this from happening again, Arthur wants to make sure that there are at least as many women as men at this year's party. He gave you a list of integers of all the party goers.
+
+// Arthur needs you to return true if he needs to invite more women or false if he is all set.
+
+// Input/Output
+// [input] integer array L ($a in PHP)
+
+// An array (guaranteed non-associative in PHP) representing the genders of the attendees, where -1 represents women and 1 represents men.
+
+// 2 <= L.length <= 50
+
+// [output] a boolean value
+
+// true if Arthur need to invite more women, false otherwise.
+
+let inviteMoreWomen = L => L.reduce((a,b) => a+b) > 0;
