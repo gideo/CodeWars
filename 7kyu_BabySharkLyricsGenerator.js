356f577007543ca90e6e9e9524b29059497f3acd
Tong Lin <brooklyntechlin@gmail.com>
2019-07-16 06:01:35 -0400

    added 7kyu - Baby Shark Lyrics Generator

---
+++ 7kyu_BabySharkLyricsGenerator.js
@@ -0,0 +1,36 @@
+// 7kyu - Baby Shark Lyrics Generator
+
+// Create a function, as short as possible, that returns this lyrics.
+// Your code should be less than 300 characters.
+
+// Baby shark, doo doo doo doo doo doo
+// Baby shark, doo doo doo doo doo doo
+// Baby shark, doo doo doo doo doo doo
+// Baby shark!
+// Mommy shark, doo doo doo doo doo doo
+// Mommy shark, doo doo doo doo doo doo
+// Mommy shark, doo doo doo doo doo doo
+// Mommy shark!
+// Daddy shark, doo doo doo doo doo doo
+// Daddy shark, doo doo doo doo doo doo
+// Daddy shark, doo doo doo doo doo doo
+// Daddy shark!
+// Grandma shark, doo doo doo doo doo doo
+// Grandma shark, doo doo doo doo doo doo
+// Grandma shark, doo doo doo doo doo doo
+// Grandma shark!
+// Grandpa shark, doo doo doo doo doo doo
+// Grandpa shark, doo doo doo doo doo doo
+// Grandpa shark, doo doo doo doo doo doo
+// Grandpa shark!
+// Let's go hunt, doo doo doo doo doo doo
+// Let's go hunt, doo doo doo doo doo doo
+// Let's go hunt, doo doo doo doo doo doo
+// Let's go hunt!
+// Run away,…
+// Good Luck!
+
+function babySharkLyrics(){
+  let arr = ["Baby shark", "Mommy shark", "Daddy shark", "Grandma shark", "Grandpa shark", "Let's go hunt"];
+  return arr.map((name)=> `${name},${" doo".repeat(6)}\n`.repeat(3) + `${name}!\n`).join("") + "Run away,…";
+}
