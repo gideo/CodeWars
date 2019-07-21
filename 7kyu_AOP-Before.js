8084cd12d30e32b2eb464699f789d2e44baee472
Tong Lin <brooklyntechlin@gmail.com>
2019-07-21 06:47:13 -0400

    added 7kyu - AOP-Before

---
+++ 7kyu_AOP-Before.js
@@ -0,0 +1,19 @@
+// 7kyu - AOP-Before
+
+// Aspect-oriented programming (AOP) is programming that adds additional behavior (advice) to existing functionality without actually modifying that functionality.
+
+// Create a method called adviseBefore. This method will take two arguments, a function (target) and the advising function (advice).
+
+// adviseBefore should return a function that, when executed, will first execute the advising function and then the original method with the following conditions:
+
+// The arguments passed to the function that adviseBefore returns should be passed to the advising function.
+// If the advising function returns an array, the array should replace the arguments passed to the original method.
+// If the advising function does not return an array, the original arguments should be passed to the original method.
+// The return value of the original method should be returned.
+
+function adviseBefore(t, a) {
+  return function(...args) {
+    let r = a(...args);
+    return Array.isArray(r)?t(...r):t(...args);
+  }
+}
