b4b7a7753c9a5b063f6f1f823fd10c44d4166fe7
Tong Lin <brooklyntechlin@gmail.com>
2019-07-29 06:42:58 -0400

    added 7kyu - Nice Array

---
+++ 7kyu_NiceArray.js
@@ -0,0 +1,15 @@
+// 7kyu - Nice Array
+
+// A Nice array is defined to be an array where for every value n in the array, there is also an element n-1 or n+1 in the array.
+
+// example:
+
+// [2,10,9,3] is Nice array because
+
+// 2=3-1
+// 10=9+1
+// 3=2+1
+// 9=10-1
+// Write a function named isNice/IsNice that returns true if its array argument is a Nice array, else false. You should also return false if input array has no elements.
+
+let isNice = a => !!a.length && a.every(x => a.some(y => y === x - 1 ||  y === x + 1));
