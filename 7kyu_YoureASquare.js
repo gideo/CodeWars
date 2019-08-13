3130185626b810f725c838263a5144beb568b829
Tong Lin <brooklyntechlin@gmail.com>
2019-08-13 04:28:34 -0400

    added 7kyu - You're a Square

---
+++ 7kyu_YoureASquare.js
@@ -0,0 +1,23 @@
+// 7kyu - You're a Square!
+
+// A square of squares
+// You like building blocks. You especially like building blocks that are squares. And what you even like more, is to arrange them into a square of square building blocks!
+
+// However, sometimes, you can't arrange them into a square. Instead, you end up with an ordinary rectangle! Those blasted things! If you just had a way to know, whether you're currently working in vain… Wait! That's it! You just have to check if your number of building blocks is a perfect square.
+
+// Task
+// Given an integral number, determine if it's a square number:
+
+// In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+
+// The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+
+// Examples
+// isSquare(-1) returns  false
+// isSquare(0) returns   true
+// isSquare(3) returns   false
+// isSquare(4) returns   true
+// isSquare(25) returns  true  
+// isSquare(26) returns  false
+
+let isSquare = n => Math.sqrt(n) % 1 === 0;
