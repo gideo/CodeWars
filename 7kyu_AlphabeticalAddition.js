825fdd6ed13a04ab4c060ac40a5f791d8e358b4d
Tong Lin <brooklyntechlin@gmail.com>
2019-08-29 05:55:20 -0400

    added 7kyu - Alphabetical Addition

---
+++ 7kyu_AlphabeticalAddition.js
@@ -0,0 +1,19 @@
+// 7kyu - Alphabetical Addition
+
+// Your task is to add up letters to one letter.
+
+// The function will be given a variable amount of arguments, each one being a letter to add.
+
+// Notes:
+// Letters will always be lowercase.
+// Letters can overflow (see second to last example of the description)
+// No letters should return 'z'
+// Examples:
+// addLetters('a', 'b', 'c') = 'f'
+// addLetters('a', 'b') = 'c'
+// addLetters('z') = 'z'
+// addLetters('z', 'a') = 'a'
+// addLetters('y', 'c', 'b') = 'd' // notice the letters overflowing
+// addLetters() = 'z'
+
+let addLetters = (...letters) => String.fromCharCode((letters.reduce((sum, l) => sum + l.charCodeAt() - 96, 0) % 26 || 26) + 96)
