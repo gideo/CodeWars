77af90f0e2a40787e5e85ecbca1563454b5a6ae6
Tong Lin <brooklyntechlin@gmail.com>
2019-08-30 05:58:18 -0400

    added 8kyu - Get Nth Even Number

---
+++ 8kyu_GetNthEvenNumber.js
@@ -0,0 +1,12 @@
+// 8kyu - Get Nth Even Number
+
+// Return the Nth Even Number
+
+// nthEven(1) //=> 0, the first even number is 0
+// nthEven(3) //=> 4, the 3rd even number is 4 (0, 2, 4)
+
+// nthEven(100) //=> 198
+// nthEven(1298734) //=> 2597466
+// The input will not be 0.
+
+let nthEven=n=>n*2-2;
