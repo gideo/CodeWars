2149ff925539baa8ad58b5d83df9a7558c2773c3
Tong Lin <brooklyntechlin@gmail.com>
2019-09-20 06:36:39 -0400

    added 8kyu - Generate Range of Integers

---
+++ 8kyu_GenerateRangeOfIntegers.js
@@ -0,0 +1,21 @@
+// 8kyu - Generate Range of Integers
+
+// Implement a function named generateRange(min, max, step), which takes three arguments and generates a range of integers from min to max, with the step. The first integer is the minimum value, the second is the maximum of the range and the third is the step. (min < max)
+
+// Task
+// Implement a function named
+
+// generateRange(2, 10, 2) // should return array of [2,4,6,8,10]
+// generateRange(1, 10, 3) // should return array of [1,4,7,10]
+// Note
+// min < max
+// step > 0
+// the range does not HAVE to include max (depending on the step)
+
+function generateRange(min, max, step){
+  let arr = [];
+  for (let i=min; i<=max; i += step) {
+    arr.push(i);
+  }
+  return arr;
+}
