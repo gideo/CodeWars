f4e0ca49d81aec937929ad33050fa7a89be74639
Tong Lin <brooklyntechlin@gmail.com>
2019-09-22 05:23:02 -0400

    added 7kyu - Thinking & Testing - How Many Word

---
+++ 7kyu_Thinking&Testing_HowManyWord.js
@@ -0,0 +1,11 @@
+// 7kyu - Thinking & Testing: How many "word"
+
+// No Story
+
+// No Description
+
+// Only by Thinking and Testing
+
+// Look at result of testcase, guess the code!
+
+let testit = s => (s.match(/w.*?o.*?r.*?d/ig) || []).length;
