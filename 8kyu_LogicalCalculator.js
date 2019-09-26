49f3153a27cfd97e489ba4c50b80bfaff90cdca2
Tong Lin <brooklyntechlin@gmail.com>
2019-09-26 06:36:56 -0400

    added 8kyu - Logical Calculator

---
+++ 8kyu_LogicalCalculator.js
@@ -0,0 +1,47 @@
+// 8kyu - Logical Calculator
+
+// Your task is to calculate logical value of boolean array. Test arrays are one-dimensional and their size is in the range 1-50.
+
+// Links referring to logical operations: AND, OR and XOR.
+
+// You should begin at the first value, and repeatedly apply the logical operation across the remaining elements in the array sequentially.
+
+// First Example:
+
+// Input: true, true, false, operator: AND
+
+// Steps: true AND true -> true, true AND false -> false
+
+// Output: false
+
+// Second Example:
+
+// Input: true, true, false, operator: OR
+
+// Steps: true OR true -> true, true OR false -> true
+
+// Output: true
+
+// Third Example:
+
+// Input: true, true, false, operator: XOR
+
+// Steps: true XOR true -> false, false XOR false -> false
+
+// Output: false
+
+// Input:
+
+// boolean array, string with operator' s name: 'AND', 'OR', 'XOR'.
+
+// Output:
+
+// calculated boolean
+
+let ops = {
+  'AND': (a, b) => a && b,
+  'OR': (a, b) =>  a || b,
+  'XOR': (a, b) => a !== b
+}
+
+let logicalCalc = (array, op) => array.reduce(ops[op]);
