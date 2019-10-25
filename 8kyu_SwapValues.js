4880c3762abddf419f18b83e584671bca63b9417
Tong Lin <brooklyntechlin@gmail.com>
2019-10-25 06:15:31 -0400

    added 8kyu - Swap Values

---
+++ 8kyu_SwapValues.js
@@ -0,0 +1,9 @@
+// 8kyu - Swap Values
+
+// I would like to be able to pass an array with two elements to my swapValues function to swap the values. However it appears that the values aren't changing.
+
+// Can you figure out what's wrong here?
+
+function swapValues() {
+  return arguments[0].reverse();
+}
