8964bfe146b039d143c4db1192c9a1488e2b81db
Tong Lin <brooklyntechlin@gmail.com>
2019-11-04 05:34:59 -0500

    added 7kyu - Complete The Pattern #7 - Cyclical Permutation

---
+++ 7kyu_CompleteThePattern#7_CyclicalPermutation.js
@@ -0,0 +1,32 @@
+// 7kyu - Complete The Pattern #7 - Cyclical Permutation
+
+// Task:
+// You have to write a function pattern which creates the following pattern (See Examples) upto desired number of rows.
+
+// If the Argument is 0 or a Negative Integer then it should return "" i.e. empty string.
+
+// Examples:
+// pattern(9):
+
+// 123456789
+// 234567891
+// 345678912
+// 456789123
+// 567891234
+// 678912345
+// 789123456
+// 891234567
+// 912345678
+// pattern(5):
+
+// 12345
+// 23451
+// 34512
+// 45123
+// 51234
+// Note: There are no spaces in the pattern
+
+// Hint: Use \n in string to jump to next line
+
+let pattern = n => Array.from({ length: n }, (x, i) =>
+    Array.from({ length: n }, (x, j) => (i + j) % n + 1).join("")).join("\n");
