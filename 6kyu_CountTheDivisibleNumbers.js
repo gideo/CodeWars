405cb67279eee7223601a1087b6be0e889562dbc
Tong Lin <brooklyntechlin@gmail.com>
2019-11-19 06:58:58 -0500

    added 6kyu - Count the Divisible Numbers

---
+++ 6kyu_CountTheDivisibleNumbers.js
@@ -0,0 +1,9 @@
+// 6kyu - Count the Divisible Numbers
+
+// Write function divisibleCount(x, y, k) that takes in 3 numbers x, y and k, and returns the number of integers within the range [x..y] that are divisible by k i.e.:
+// { i : x ≤ i ≤ y, i mod k = 0 }
+// For example:
+// x = 6, y = 11 and k = 2, your function should return 3, because there are three numbers divisible by 2 within the range [6..11], namely 6, 8 and 10.
+// Note: x<=y
+
+let divisibleCount = (x,y,k) => Math.floor(y/k) - Math.floor((x-1)/k);
