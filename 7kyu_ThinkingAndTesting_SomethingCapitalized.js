c069eeb1d4f8bf0077dc56197552a547062ea2ed
Tong Lin <brooklyntechlin@gmail.com>
2019-12-17 06:03:43 -0500

    added 7kyu - Thinking and Testing - Something Capitalized

---
+++ 7kyu_ThinkingAndTesting_SomethingCapitalized.js
@@ -0,0 +1,11 @@
+// 7kyu - Thinking & Testing : Something capitalized
+
+// No Story
+
+// No Description
+
+// Only by Thinking and Testing
+
+// Look at result of testcase, guess the code!
+
+let testit = s => s.replace(/.\b/g, c => c.toUpperCase());
