aedc6b69afc5e6a756c564e89b88ba3e93697be3
Tong Lin <brooklyntechlin@gmail.com>
2019-12-20 05:21:41 -0500

    added 7kyu - Substring Fun

---
+++ 7kyu_SubstringFun.js
@@ -0,0 +1,14 @@
+// 7kyu - Substring Fun
+
+// Complete the function that takes an array of words.
+
+// You must concatenate the nth letter from each word to construct a new word which should be returned as a string, where n is the position of the word in the list.
+
+// For example:
+
+// ["yoda", "best", "has"]  -->  "yes"
+//   ^        ^        ^
+//   n=0     n=1     n=2
+// Note: Test cases contain valid input only - i.e. a string array or an empty array; and each word will have enough letters.
+
+let nthChar = words => words.map((word, index)=>word[index]).join('');
