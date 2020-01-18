305b9e8afe0c9533ee1686354652264a13da2605
Tong Lin <brooklyntechlin@gmail.com>
2020-01-18 06:08:14 -0500

    added 7kyu - Calculate Parity Bit

---
+++ 7kyu_CalculateParityBit.js
@@ -0,0 +1,21 @@
+// 7kyu - Calculate Parity Bit  
+
+// You have the parameters parity and bin.
+// Parity is always 'even' or 'odd'.
+// Your task is to return an integer (0 or 1).
+
+// A parity bit, or check bit, is a bit added to a string of bits to ensure that the total number of 1-bits in the string is even or odd. Parity bits are used as the simplest form of error detecting code.
+
+// Example:
+
+//   Parity: 'even'
+//   Bin: '0101010'
+//   Result: 1
+// Because there is an odd number of 1-bits (3) you need to put another 1 to it to get an even number of 1-bits.
+
+// For more information: https://en.wikipedia.org/wiki/Parity_bit
+
+
+function checkParity(ﾠ,[...ﾠﾠ]){
+  return ﾠ=='even'?(ﾠﾠ.filter(x=>x=='1').length%2?1:0):(ﾠﾠ.filter(x=>x=='1').length%2?0:1);
+}
