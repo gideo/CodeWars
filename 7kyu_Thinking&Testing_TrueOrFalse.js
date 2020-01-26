8e1d2de949dfe9467b2ad81919103430e655e98a
Tong Lin <brooklyntechlin@gmail.com>
2020-01-26 05:47:00 -0500

    added 7kyu - Thinking & Testing - True or False

---
+++ 7kyu_Thinking&Testing_TrueOrFalse.js
@@ -0,0 +1,11 @@
+// 7kyu - Thinking & Testing: True or False
+
+// No Story
+
+// No Description
+
+// Only by Thinking and Testing
+
+// Look at result of testcase, guess the code!
+
+const testit = n => n.toString(2).replace(/0/g, '').length;
