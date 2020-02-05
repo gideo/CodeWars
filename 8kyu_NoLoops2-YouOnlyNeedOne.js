bdf4c1bd57b35ab6766d527e83efc72f520ba437
Tong Lin <brooklyntechlin@gmail.com>
2020-02-05 05:31:42 -0500

    added 8kyu - No Loops 2 - You Only Need One

---
+++ 8kyu_NoLoops2-YouOnlyNeedOne.js
@@ -0,0 +1,15 @@
+// 8kyu - No Loops 2 - You only need one
+
+// * No Loops Allowed *
+
+// You will be given an array (a) and a value (x). All you need to do is check whether the provided array contains the value, without using a loop.
+
+// Array can contain numbers or strings. X can be either. Return true if the array contains the value, false if not. With strings you will need to account for case.
+
+// Looking for more, loop-restrained fun? Check out the other kata in the series:
+
+// https://www.codewars.com/kata/no-loops-1-small-enough
+
+// https://www.codewars.com/kata/no-loops-3-copy-within
+
+let check = (a,b) => a.includes(b);
