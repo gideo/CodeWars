674b8f250ae35941eac6e6309eb976103b8d8ed3
Tong Lin <brooklyntechlin@gmail.com>
2020-02-10 06:04:27 -0500

    added 8kyu - Area of Perimeter

---
+++ 8kyu_AreaOfPerimeter.js
@@ -0,0 +1,12 @@
+// 8kyu - Area or Perimeter
+
+// You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+// If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+// area_or_perimeter(6, 10) --> 32
+// area_or_perimeter(4, 4) --> 16
+// Note: for the purposes of this kata you will assume that it is a square if its length and width are equal, otherwise it is a rectangle.
+
+const areaOrPerimeter = function(l , w) {
+  return l == w ? l*w : 2*(l + w)
+};
