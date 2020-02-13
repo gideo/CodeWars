714ed72823a8e7849da475b774cff48e726acc0a
Tong Lin <brooklyntechlin@gmail.com>
2020-02-13 05:42:06 -0500

    added 8kyu - Beginner Series #1 - School Paperwork

---
+++ 8kyu_BeginnerSeries#1_SchoolPaperwork.js
@@ -0,0 +1,11 @@
+// 8kyu - Beginner Series #1 School Paperwork
+
+// Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+// Your task is to calculate how many blank pages do you need.
+
+// Example:
+// paperwork(5, 5) == 25
+// Note! if n or m < 0 return 0! Waiting for translations and Feedback! Thanks!
+
+let paperwork = (n,m) => n > 0 && m > 0 ? n * m : 0;
