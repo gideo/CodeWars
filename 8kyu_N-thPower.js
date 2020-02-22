b1ff9e26f94618e89bdd408b62e542b7d928099a
Tong Lin <brooklyntechlin@gmail.com>
2020-02-22 05:15:45 -0500

    added 8kyu - N-th Power

---
+++ 8kyu_N-thPower.js
@@ -0,0 +1,12 @@
+// 8kyu - N-th Power
+
+// This kata is from check py.checkio.org
+
+// You are given an array with positive numbers and a number N. You should find the N-th power of the element in the array with the index N. If N is outside of the array, then return -1. Don't forget that the first element has the index 0.
+
+// Let's look at a few examples:
+
+// array = [1, 2, 3, 4] and N = 2, then the result is 3^2 == 9;
+// array = [1, 2, 3] and N = 3, but N is outside of the array, so the result is -1.
+
+let index = (arr, n) => arr.length > n ? Math.pow(arr[n], n) : -1;
