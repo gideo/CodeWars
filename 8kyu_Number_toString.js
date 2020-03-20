f7e0801b3f9c5baf64374ee117ec12a2f75cf0f6
Tong Lin <brooklyntechlin@gmail.com>
2020-03-20 06:50:12 -0400

    added 8kyu - Number toString

---
+++ 8kyu_Number_toString.js
@@ -0,0 +1,11 @@
+// 8kyu - Number toString 
+
+// The code gives an error!
+
+// a = 123.toString
+// Fix it!
+
+var a = 123..toString();
+var a = 123 .toString()
+var a = (123).toString();
+var a = 123 + '';
