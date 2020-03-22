ed10f59bf24a2cee1d8d6ede9e3c53e150add3f2
Tong Lin <brooklyntechlin@gmail.com>
2020-03-22 06:25:25 -0400

    added 7kyu - Geometry Basics - Dot Product in 3D

---
+++ 7kyu_GeometryBasics_DotProductIn3D.js
@@ -0,0 +1,15 @@
+// 7kyu - Geometry Basics: Dot Product in 3D 
+
+// This series of katas will introduce you to basics of doing geometry with computers.
+
+// Vector objects have x, y, and z attributes.
+
+// Write a function calculating dot product of Vector a and Vector b.
+
+// You can read more about dot product on Wikipedia.
+
+// Tests round answers to 6 decimal places.
+
+// FUNDAMENTALSGEOMETRYALGEBRAMAT
+
+let dotProduct = (v1, v2) => v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
