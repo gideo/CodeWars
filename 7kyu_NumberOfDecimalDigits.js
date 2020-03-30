548ebdd28a777563768e00efcfc9167293084bfd
Tong Lin <brooklyntechlin@gmail.com>
2020-03-30 06:58:29 -0400

    added 7kyu - Number of Decimal Digits

---
+++ 7kyu_NumberOfDecimalDigits.js
@@ -0,0 +1,9 @@
+// 7kyu - Number of Decimal Digits 
+
+// Determine the total number of digits in the integer (n>=0) given as input to the function. For example, 9 is a single digit, 66 has 2 digits and 128685 has 6 digits. Be careful to avoid overflows/underflows.
+
+// All inputs will be valid.
+
+function digits(n) {
+  return String(n).length;
+}
