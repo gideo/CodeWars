afef7a0ea12a20324be49b4d35a9807034b398f6
Tong Lin <brooklyntechlin@gmail.com>
2020-04-05 06:54:08 -0400

    added 8kyu - Polish Alphabet

---
+++ 8kyu_PolishAlphabet.js
@@ -0,0 +1,25 @@
+// 8kyu - Polish Alphabet 
+
+// There are 32 letters in the Polish alphabet: 9 vowels and 23 consonants.
+
+// Your task is to change the letters with diacritics:
+
+// ą -> a,
+// ć -> c,
+// ę -> e,
+// ł -> l,
+// ń -> n,
+// ó -> o,
+// ś -> s,
+// ź -> z,
+// ż -> z
+// and print out the string without the use of the Polish letters.
+
+// For example:
+
+// "Jędrzej Błądziński"  -->  "Jedrzej Bladzinski"
+
+function correctPolishLetters (string) {
+  var dict = {'ą':'a','ć':'c','ę':'e','ł':'l','ń':'n','ó':'o','ś':'s','ź':'z','ż':'z'};
+  return string.replace(/[ąćęłńóśźż]/g, match => dict[match]);
+}
