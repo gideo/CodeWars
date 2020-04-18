1203dbda98d51422395aa3d59ec1a80c9e735a26
Tong Lin <brooklyntechlin@gmail.com>
2020-04-18 06:57:35 -0400

    added 7kyu - Find Min and Max

---
+++ 7kyu_FindMinAndMax.js
@@ -0,0 +1,5 @@
+// 7kyu - Find min and max 
+
+// Implement a function that returns the minimal and the maximal value of a list (in this order).
+
+let getMinMax = arr => [Math.min(...arr),Math.max(...arr)];
