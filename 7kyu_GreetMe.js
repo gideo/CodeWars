280d3e09a2b87044b427d85227db8e4cf3f0d85d
Tong Lin <brooklyntechlin@gmail.com>
2020-05-21 06:11:02 -0400

    added 7kyu - Greet Me

---
+++ 7kyu_GreetMe.js
@@ -0,0 +1,16 @@
+// 7kyu - Greet Me 
+
+// Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+// Example:
+
+// "riley" --> "Hello Riley!"
+// "JACK"  --> "Hello Jack!"
+
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
+}
+
+var greet = function(name) {
+return "Hello " + name.capitalize() + "!";
+};
