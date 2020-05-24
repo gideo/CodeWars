305d89e0b3ddfda190c993789d0918a50650477a
Tong Lin <brooklyntechlin@gmail.com>
2020-05-24 06:30:37 -0400

    added 6kyu - Encrypt This

---
+++ 6kyu_EncryptThis.js
@@ -0,0 +1,21 @@
+// 6kyu - Encrypt This! 
+
+// Acknowledgments:
+// I thank yvonne-liu for the idea and for the example tests :)
+
+// Description:
+// Encrypt this!
+
+// You want to create secret messages which can be deciphered by the Decipher this! kata. Here are the conditions:
+
+// Your message is a string containing space separated words.
+// You need to encrypt each word in the message using the following rules:
+// The first letter needs to be converted to its ASCII code.
+// The second letter needs to be switched with the last letter
+// Keepin' it simple: There are no special characters in input.
+// Examples:
+// encryptThis("Hello") === "72olle"
+// encryptThis("good") === "103doo"
+// encryptThis("hello world") === "104olle 119drlo"
+
+let encryptThis = text => text.replace(/\b\w(\w?)(\w*?)(\w?)\b/g, (word, a, mid, b) => word.charCodeAt(0) + b + mid + a);
