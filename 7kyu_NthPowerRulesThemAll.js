7c0a415b469a56712367ff6cae6f3d246f45791b
Tong Lin <brooklyntechlin@gmail.com>
2020-06-12 05:21:24 -0400

    added 7kyu - Nth Power Rules Them All

---
+++ 7kyu_NthPowerRulesThemAll.js
@@ -0,0 +1,11 @@
+// 7kyu - Nth Power Rules them All 
+
+// You are provided with array of positive non-zero ints and int n representing n-th power (n >= 2).
+
+// For the given array, calculate the sum of each value to the n-th power. Then subtract the sum of the original array.
+
+// Example 1: Input: {1, 2, 3}, 3 --> (1 ^ 3 + 2 ^ 3 + 3 ^ 3 ) - (1 + 2 + 3) --> 36 - 6 --> Output: 30
+
+// Example 2: Input: {1, 2}, 5 --> (1 ^ 5 + 2 ^ 5) - (1 + 2) --> 33 - 3 --> Output: 30
+
+let modifiedSum = (a,n) => a.reduce((s, c) => s + Math.pow(c, n) - c, 0);
