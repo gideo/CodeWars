39fbaf56524b2a53a1ecbb52037ceb5effa7e0dd
Tong Lin <brooklyntechlin@gmail.com>
2020-07-01 06:09:12 -0400

    added 7kyu - Array Element Parity

---
+++ 7kyu_ArrayElementParity.js
@@ -0,0 +1,21 @@
+// 7kyu - Array Element Parity 
+
+// In this Kata, you will be given an array of integers whose elements have both a negative and a positive value, except for one integer that is either only negative or only positive. Your task will be to find that integer.
+
+// Examples:
+
+// [1, -1, 2, -2, 3] => 3
+
+// 3 has no matching negative appearance
+
+// [-3, 1, 2, 3, -1, -4, -2] => -4
+
+// -4 has no matching positive appearance
+
+// [1, -1, 2, -2, 3, 3] => 3
+
+// (the only-positive or only-negative integer may appear more than once)
+
+// Good luck!
+
+let solve = arr => [...new Set(arr)].reduce((a, b) => a + b);
