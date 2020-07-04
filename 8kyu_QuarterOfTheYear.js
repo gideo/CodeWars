eb6c0de8599c3110da720c2dfdca5d9cc0bbb32e
Tong Lin <brooklyntechlin@gmail.com>
2020-07-04 05:45:21 -0400

    added 8kyu - Quarter of the Year

---
+++ 8kyu_QuarterOfTheYear.js
@@ -0,0 +1,9 @@
+// 8kyu - Quarter of the Year 
+
+// Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+// For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+
+// FUNDAMENTALS
+
+let quarterOf = m => Math.ceil(m/3);
