dbcd2432ddd5941d8ade22a889588c52bc7b8e61
Tong Lin <brooklyntechlin@gmail.com>
2020-08-08 06:37:06 -0400

    added 8kyu - Grasshoper - Function Syntax Debugging

---
+++ 8kyu_Grasshopper_FunctionSyntaxDebugging.js
@@ -0,0 +1,6 @@
+// 8kyu - Grasshopper - Function Syntax Debugging 
+
+// Grasshopper - Function syntax debugging
+// A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+let main = (verb,noun) => verb + noun;
