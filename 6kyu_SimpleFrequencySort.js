dcf304905d74949c1b9dc8ef2396837d00d7d3e6
Tong Lin <brooklyntechlin@gmail.com>
2020-08-17 06:31:59 -0400

    added 6kyu - Simple Frequency Sort

---
+++ 6kyu_SimpleFrequencySort.js
@@ -0,0 +1,16 @@
+// 6kyu - Simple Frequency Sort 
+
+// In this Kata, you will sort elements in an array by decreasing frequency of elements. If two elements have the same frequency, sort them by increasing value.
+
+// solve([2,3,5,3,7,9,5,3,7]) = [3,3,3,5,5,7,7,2,9]
+// --we sort by highest frequency to lowest frequency. If two elements have same frequency, we sort by increasing value
+// More examples in test cases.
+
+// Good luck!
+
+// Please also try Simple time difference
+
+function solve(arr){
+  let h = arr.reduce((h,n)=>(h[n]=h[n]+1||1,h),{});
+  return arr.sort((a,b)=>h[b]-h[a]||a-b);
+}
