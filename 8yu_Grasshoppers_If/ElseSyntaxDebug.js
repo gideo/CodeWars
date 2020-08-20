009089771f0172e158604636bf56acdaea6447d4
Tong Lin <brooklyntechlin@gmail.com>
2020-08-20 06:27:27 -0400

    added 8kyu - Grasshopper_If/Else Syntax Debug

---
+++ 8yu_Grasshoppers_If/ElseSyntaxDebug.js
@@ -0,0 +1,10 @@
+// 8kyu - Grasshopper - If/else Syntax Debug 
+
+// If/else syntax debug
+// While making a game, your partner, Greg, decided to create a function to check if the user is still alive called checkAlive/CheckAlive/check_alive. Unfortunately, Greg made some errors while creating the function.
+
+// checkAlive/CheckAlive/check_alive should return true if the player's health is greater than 0 or false if it is 0 or below.
+
+// The function receives one parameter health which will always be a whole number between -10 and 10.
+
+let checkAlive = health => health > 0;
