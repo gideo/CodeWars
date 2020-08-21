a25c1b890085aec8055287a9e81e2bfa738ef9c7
Tong Lin <brooklyntechlin@gmail.com>
2020-08-21 06:26:25 -0400

    added 7kyu - Pluck

---
+++ 7kyu_Pluck.js
@@ -0,0 +1,13 @@
+// 7kyu - Pluck 
+
+// Implement a function which takes a sequence of objects and a property name, and returns a sequence containing the named property of each object.
+
+// For example:
+
+// pluck([{a:1}, {a:2}], 'a')      // -> [1,2]
+// pluck([{a:1, b:3}, {a:2}], 'b') // -> [3, undefined]
+// If an object is missing the property, you should just leave it as undefined/None in the output array.
+
+function pluck(objs, name) {
+  return objs.map(function(obj) { return obj[name] });
+}
