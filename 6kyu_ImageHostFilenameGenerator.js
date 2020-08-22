b46db0287c81586325c754f9708b88528c5041ee
Tong Lin <brooklyntechlin@gmail.com>
2020-08-22 06:28:44 -0400

    added 6kyu - Image Host Filename Generator

---
+++ 6kyu_ImageHostFilenameGenerator.js
@@ -0,0 +1,18 @@
+// 6kyu - Image Host Filename Generator
+
+// You are developing an image hosting website.
+
+// You have to create a function for generating random and unique image filenames.
+
+// Create a function for generating a random 6 character string which will be used to access the photo URL.
+
+// To make sure the name is not already in use, you are given access to an PhotoManager object.
+
+// You can call it like so to make sure the name is unique
+
+// // at this point, the website has only one photo, hosted on the 'ABCDEF' url
+// photoManager.nameExists('ABCDEF'); // returns true
+// photoManager.nameExists('BBCDEF'); // returns false
+// Note: We consider two names with same letters but different cases to be unique.
+
+let generateName = () => Math.random().toString(36).slice(2,8);
