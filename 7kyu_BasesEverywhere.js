33ea8840763e7467c5c8efa14c13e2a7f80e8cbf
Tong Lin <brooklyntechlin@gmail.com>
2020-09-02 06:39:03 -0400

    added 7kyu - Bases Everywhere

---
+++ 7kyu_BasesEverywhere.js
@@ -0,0 +1,21 @@
+// 7kyu - Bases Everywhere
+
+// Uh oh, Someone at the office has dropped all these sequences on the floor and forgotten to label them with their correct bases.
+
+// We have to fix this before the boss gets back or we're all going to be fired!
+
+// This is what your years of coding have been leading up to, now is your time to shine!
+
+// Task:
+// You will have to create a function which takes in a sequence of numbers in random order and you will have to return the correct base of those numbers.
+
+// Constraints:
+// The sequence will always be 10 numbers long and we know that the base is going to be between 2 and 10 inclusive so no need to worry about any letters. When sorted, the sequence is made up of consecutive numbers.
+
+// Example:
+// baseFinder(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']) === 10 //evaluates to true
+// This is because all the numbers above when ordered fit the base 10 format
+
+// Good luck! 
+
+let baseFinder = seq => new Set(seq.join("")).size;
