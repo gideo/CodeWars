aec4ad7d7c7b13afaaff724702a3f7cb11db6be6
Tong Lin <brooklyntechlin@gmail.com>
2020-09-23 06:34:29 -0400

    added 7kyu - Simple Consecutive Pairs

---
+++ 7kyu_SimpleConsecutivePairs.js
@@ -0,0 +1,23 @@
+// 7kyu - Simple Consecutive Pairs 
+
+// In this Kata your task will be to return the count of pairs that have consecutive numbers as follows:
+
+// pairs([1,2,5,8,-4,-3,7,6,5]) = 3
+// The pairs are selected as follows [(1,2),(5,8),(-4,-3),(7,6),5]
+// --the first pair is (1,2) and the numbers in the pair are consecutive; Count = 1
+// --the second pair is (5,8) and are not consecutive
+// --the third pair is (-4,-3), consecutive. Count = 2
+// --the fourth pair is (7,6), also consecutive. Count = 3. 
+// --the last digit has no pair, so we ignore.
+// More examples in the test cases.
+
+// Good luck!
+
+// Please also try Simple time difference
+
+function pairs(array, count = 0) {
+  for (let i = 0; i < array.length; i += 2)
+    if (Math.abs(array[i] - array[i + 1]) === 1)
+      count += 1;
+  return count;
+}
