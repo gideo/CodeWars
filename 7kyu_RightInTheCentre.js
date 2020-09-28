c860d69b133f732ab295b9cd236a297cbdac00d9
Tong Lin <brooklyntechlin@gmail.com>
2020-09-28 06:21:13 -0400

    added 7kyu - Right in the Centre

---
+++ 7kyu_RightInTheCentre.js
@@ -0,0 +1,26 @@
+// 7kyu - Right in the Centre 
+
+// Right in the Center
+// This is inspired by one of Nick Parlante's exercises on the CodingBat online code practice tool.
+
+// Given a sequence of characters, does "abc" appear in the CENTER of the sequence?
+
+// The sequence of characters could contain more than one "abc".
+
+// To define CENTER, the number of characters in the sequence to the left and right of the "abc" (which is in the middle) must differ by at most one.
+
+// If it is in the CENTER, return True. Otherwise, return False.
+
+// Write a function as the solution for this problem. This kata looks simple, but it might not be easy.
+
+// Example
+// is_in_middle("AAabcBB")  ->  True
+// is_in_middle("AabcBB")   ->  True
+// is_in_middle("AabcBBB")  ->  False
+
+function isInMiddle(seq) {
+  let center=(seq.length-3)/2,
+      a = seq.substring(center,center+3+((seq.length-3)%2));
+  if (a.indexOf("abc")==-1) return false;
+  return true;
+}
