f7910be0f2407538872671fbf7751c590071964d
Tong Lin <brooklyntechlin@gmail.com>
2020-10-23 07:08:11 -0400

    added 6kyu - Matrix Addition

---
+++ 6kyu_MatrixAddition.js
@@ -0,0 +1,35 @@
+// 6kyu - Matrix Addition 
+
+// Write a function that accepts two square matrices (N x N two dimensional arrays), and return the sum of the two. Both matrices being passed into the function will be of size N x N (square), containing only integers.
+
+// How to sum two matrices:
+
+// Take each cell [n][m] from the first matrix, and add it with the same [n][m] cell from the second matrix. This will be cell [n][m] of the solution matrix.
+
+// Visualization:
+
+// |1 2 3|     |2 2 1|     |1+2 2+2 3+1|     |3 4 4|
+// |3 2 1|  +  |3 2 3|  =  |3+3 2+2 1+3|  =  |6 4 4|
+// |1 1 1|     |1 1 3|     |1+1 1+1 1+3|     |2 2 4|
+// Example
+// matrixAddition(
+//   [ [1, 2, 3],
+//     [3, 2, 1],
+//     [1, 1, 1] ],
+// //      +
+//   [ [2, 2, 1],
+//     [3, 2, 3],
+//     [1, 1, 3] ] )
+
+// // returns:
+//   [ [3, 4, 4],
+//     [6, 4, 4],
+//     [2, 2, 4] ]
+
+function matrixAddition(a, b){
+  return a.map(function(n, i){
+    return n.map(function(o, j){
+      return o + b[i][j];
+    });
+  });
+}
