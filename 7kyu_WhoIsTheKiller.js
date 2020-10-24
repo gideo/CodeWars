6376ecc6c0eb72910b3920227b92d1eee9676eec
Tong Lin <brooklyntechlin@gmail.com>
2020-10-24 07:18:29 -0400

    added 7kyu - Who Is the Killer

---
+++ 7kyu_WhoIsTheKiller.js
@@ -0,0 +1,18 @@
+// 7kyu - Who is the Killer? 
+
+// Who is the killer?
+// Some people have been killed!
+// You have managed to narrow the suspects down to just a few. Luckily, you know every person who those suspects have seen on the day of the murders.
+
+// Task.
+// Given a dictionary with all the names of the suspects and everyone that they have seen on that day which may look like this:
+
+// {'James': ['Jacob', 'Bill', 'Lucas'],
+//  'Johnny': ['David', 'Kyle', 'Lucas'],
+//  'Peter': ['Lucy', 'Kyle']}
+// and also a list of the names of the dead people:
+
+// ['Lucas', 'Bill']
+// return the name of the one killer, in our case 'James' because he is the only person that saw both 'Lucas' and 'Bill'
+
+let killer = (suspectInfo, dead) => Object.keys(suspectInfo).find(x => dead.every(y => suspectInfo[x].includes(y)));
