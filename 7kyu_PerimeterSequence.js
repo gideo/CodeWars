e39b1af19b840e2c44fe4f18cf9c412a818e6f07
Tong Lin <brooklyntechlin@gmail.com>
2020-12-09 06:59:31 -0500

    added 7kyu - Perimeter Sequence

---
+++ 7kyu_PerimeterSequence.js
@@ -0,0 +1,12 @@
+// 7kyu - Perimeter Sequence 
+
+// Perimeter sequence
+// The first three stages of a sequence are shown.
+
+// blocks
+
+// The blocksize is a by a and a ≥ 1.
+
+// What is the perimeter of the nth shape in the sequence (n ≥ 1) ?
+
+let perimeterSequence = (a, n) => 4 * a * n;
