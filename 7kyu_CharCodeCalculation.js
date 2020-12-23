2fc11a4b116c2b15803fb7411cf0b50a8c6b5195
Tong Lin <brooklyntechlin@gmail.com>
2020-12-23 06:38:38 -0500

    added 7kyu - Char Code Calculation

---
+++ 7kyu_CharCodeCalculation.js
@@ -0,0 +1,19 @@
+// 7kyu - Char Code Calculation 
+
+// Given a string, turn each character into its ASCII character code and join them together to create a number - let's call this number total1:
+
+// 'ABC' --> 'A' = 65, 'B' = 66, 'C' = 67 --> 656667
+// Then replace any incidence of the number 7 with the number 1, and call this number 'total2':
+
+// total1 = 656667
+//               ^
+// total2 = 656661
+//               ^
+// Then return the difference between the sum of the digits in total1 and total2:
+
+//   (6 + 5 + 6 + 6 + 6 + 7)
+// - (6 + 5 + 6 + 6 + 6 + 1)
+// -------------------------
+// 						  6
+
+let calc = x =>(x.replace(/./g,x=>x.charCodeAt()).match(/7/g)||[]).length*6;
