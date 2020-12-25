cea479402e724afd75f4f4761432f5b2cd8e0d25
Tong Lin <brooklyntechlin@gmail.com>
2020-12-25 08:41:01 -0500

    added 7kyu - Construct A Bit Vector Set

---
+++ 7kyu_ConstructABitVectorSet.js
@@ -0,0 +1,28 @@
+// 7kyu - Construct a Bit Vector Set 
+
+// Task
+// Write a function sortByBit which accepts an integer array as argument and returns a 32-bit integer such that the integer, in its binary representation has 1 at only those indexes(counted from right) which are in the array.
+
+// Examples
+// sortByBit([0, 1]) // Should give integer 3
+
+// sortByBit([1, 2, 0, 4]) 
+// // Should give integer 23
+
+// Because 23 in binary is:                     
+
+
+
+//   0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 1 1 1      
+//                                             . . . . . 5 4 3 2 1 0  <--- indices     
+//                                                         |   | | |                       
+//                                                         |   | | |                       
+//                                             these bits are 1 because the corresponding indices are in the given array
+
+// FAQA: The function name is sortByBit as it simulates radix sort
+
+function sortByBit(array) {
+  return array.reduce(function(n, i) {
+    return n | (1 << i)
+  }, 0)
+}
