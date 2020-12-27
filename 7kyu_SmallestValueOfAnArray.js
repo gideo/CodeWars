eff2dee946e669a22ef911508a2bc5265cc51ada
Tong Lin <brooklyntechlin@gmail.com>
2020-12-27 05:26:51 -0500

    added 7kyu - Smallest Value of an Array

---
+++ 7kyu_SmallestValueOfAnArray.js
@@ -0,0 +1,13 @@
+// 7kyu - Smallest Value of An Array 
+
+// Write a function that can return the smallest value of an array or the index of that value. The function's 2nd parameter will tell whether it should return the value or the index.
+
+// Assume the first parameter will always be an array filled with at least 1 number and no duplicates. Assume the second parameter will be a string holding one of two values: 'value' and 'index'.
+
+// min([1,2,3,4,5], 'value') // => 1
+// min([1,2,3,4,5], 'index') // => 0
+
+function min(arr, toReturn) {
+  let val = Math.min.apply(null, arr);
+  return toReturn == 'value' ? val : arr.indexOf(val);
+}
