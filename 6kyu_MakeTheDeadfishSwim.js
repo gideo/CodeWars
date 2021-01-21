5dc1fd4a96b0e9c2430734bd0cb3aa68099e1cee
Tong Lin <brooklyntechlin@gmail.com>
2021-01-21 07:07:26 -0500

    added 6kyu - Make the Deadfish Swim

---
+++ 6kyu_MakeTheDeadfishSwim.js
@@ -0,0 +1,28 @@
+// 6kyu - Make the Deadfish Swim 
+
+// Write a simple parser that will parse and run Deadfish.
+
+// Deadfish has 4 commands, each 1 character long:
+
+// i increments the value (initially 0)
+// d decrements the value
+// s squares the value
+// o outputs the value into the return array
+// Invalid characters should be ignored.
+
+// parse("iiisdoso") => [ 8, 64 ]
+
+function parse(data) {
+  let res = [];
+
+  data.split('').reduce((cur, s) => {
+    if (s === 'i') cur++;
+    if (s === 'd') cur--;
+    if (s === 's') cur = Math.pow(cur, 2);
+    if (s === 'o') res.push(cur);
+    
+    return cur;
+  }, 0);
+  
+  return res;
+}
