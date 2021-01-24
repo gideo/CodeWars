ffd3f9ef347e5d7054ada49bbf7929b180b88817
Tong Lin <brooklyntechlin@gmail.com>
2021-01-24 06:46:08 -0500

    added 7kyu - Apparently-Modifying Strings

---
+++ 7kyu_Apparently-ModifyingStrings.js
@@ -0,0 +1,28 @@
+// 7kyu - Apparently-Modifying Strings 
+
+// For every string, after every occurrence of 'and' and/or 'but', insert the substring 'apparently' directly after the occurrence(s).
+
+// If input does not contain 'and' or 'but', return the same string. If a blank string, return ''.
+
+// If substring 'apparently' is already directly after an 'and' and/or 'but', do not add another. (Do not add duplicates).
+
+// Examples:
+// Input 1
+
+// 'It was great and I've never been on live television before but sometimes I don't watch this.'
+// Output 1
+
+// 'It was great and apparently I've never been on live television before but apparently sometimes I don't watch this.'
+// Input 2
+
+// 'but apparently'
+// Output 2
+
+// 'but apparently' 
+// (no changes because 'apparently' is already directly after 'but' and there should not be a duplicate.)
+
+// An occurrence of 'and' and/or 'but' only counts when it is at least one space separated. For example 'andd' and 'bbut' do not count as occurrences, whereas 'b but' and 'and d' does count.
+
+// reference that may help: https://www.youtube.com/watch?v=rz5TGN7eUcM
+
+let apparently = s => s.replace(/\b(and|but)\b(?! apparently\b)/g,'$& apparently');
