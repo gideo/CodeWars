32f7e6400806c6d48e2658c410820d547468b1fe
Tong Lin <brooklyntechlin@gmail.com>
2021-02-21 05:59:52 -0500

    added 8kyu - Grasshopper - Variable Assignment Debut

---
+++ 8kyu_Grasshopper_VariableAssignmentDebut.js
@@ -0,0 +1,9 @@
+// 8kyu - Grasshopper - Variable Assignment Debug 
+
+// Variable assignment
+// Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+var a = "dev"
+var b = "Lab"
+
+var name = a + b;
