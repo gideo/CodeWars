4293d3c7fe32dbfd08cd0845c310a28e708bda1c
Tong Lin <brooklyntechlin@gmail.com>
2021-02-26 06:47:53 -0500

    added 7kyu - Chuck Norris IV - Bearded Fist

---
+++ 7kyu_ChuckNorrisIV_BeardedFist.js
@@ -0,0 +1,9 @@
+// 7kyu - Chuck Norris IV - Beareded Fist 
+
+// It has long been rumoured that behind Chuck's beard is not a chin, but another fist!
+
+// When shaving, Chuck accidentally punched himself in the face. He is the only man that could take that punch without dying, but that doesn't mean it didn't sting! Chuck can't remember a thing - he needs your help!!
+
+// Hidden within the provided array of arrays are numbers that represent the letters of the words for some of Chuck's favourite things! Your job is to translate them, and return the words so that Chuck can get back to the business of punching and kicking things.
+
+let fistBeard = arr => String.fromCharCode(...[].concat(...arr));
