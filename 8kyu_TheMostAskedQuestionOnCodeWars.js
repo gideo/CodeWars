088f07bd6389a7b0db7f93e0e71d975d2119aa2a
Tong Lin <brooklyntechlin@gmail.com>
2021-03-11 06:51:41 -0500

    added 8kyu - The Most Asked Question on Code Wars

---
+++ 8kyu_TheMostAskedQuestionOnCodeWars.js
@@ -0,0 +1,11 @@
+// 8kyu - The Most Asked Question on CodeWars 
+
+// What is the most asked question on CodeWars?
+
+// Can someone explain /*...*/?
+
+// You need to write a function detect that will check if a string starts with Can someone explain, case sensitive. Return true if so, false otherwise.
+
+// Let's hope you don't write a solution that makes people ask that question at you!
+
+const detect = comment => comment.startsWith('Can someone explain');
