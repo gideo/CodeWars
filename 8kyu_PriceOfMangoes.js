5b74d8aa45aecc842948e6e119b509331573f5a2
Tong Lin <brooklyntechlin@gmail.com>
2021-04-09 06:21:30 -0400

    added 8kyu - Price of Mangoes

---
+++ 8kyu_PriceOfMangoes.js
@@ -0,0 +1,11 @@
+// 8kyu - Price of Mangoes 
+
+// There's a "3 for 2" (or "2+1" if you like) offer on mangoes. For a given quantity and price (per mango), calculate the total cost of the mangoes.
+
+// Examples
+// mango(3, 3) ==> 6    # 2 mangoes for 3 = 6; +1 mango for free
+// mango(9, 5) ==> 30   # 6 mangoes for 5 = 30; +3 mangoes for free
+
+function mango(quantity, price){
+  return price * (quantity - Math.floor(quantity / 3));
+}
