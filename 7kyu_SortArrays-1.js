a53b4e28506ac2b6097e203283ea1267f9b3c437
Tong Lin <brooklyntechlin@gmail.com>
2021-05-06 23:08:32 -0400

    added 7kyu - Sort Arrays - 1

---
+++ 7kyu_SortArrays-1.js
@@ -0,0 +1,7 @@
+// 7kyu - Sort Arrays - 1 
+
+// Just a simple sorting usage. Create a function that returns the elements of the input-array / list in a sorted manner.
+
+let sortme = function(names) {
+  return names.sort();
+}
