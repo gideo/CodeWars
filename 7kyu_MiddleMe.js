aa97943ea9abbfca2aa7c4a7f40f87042ebfa46d
Tong Lin <brooklyntechlin@gmail.com>
2021-07-14 04:44:49 -0400

    added 7kyu - Middle Me

---
+++ 7kyu_MiddleMe.js
@@ -0,0 +1,25 @@
+// 7kyu - Middle Me
+
+// Write a function that will take a key of X and place it in the middle of Y repeated N times.
+
+// Extra challege (not tested): You can complete this with under 70 characters without using regex. Challenge yourself to do this. It wont be best practices but it will work.
+
+// Rules:
+
+// If X cannot be placed in the middle, return X.
+
+// N will always be > 0.
+
+// Example:
+
+// X = 'A';
+
+// Y = '*';
+
+// N = 10;
+
+// Y repeated N times = '**********';
+
+// X in the middle of Y repeated N times = '*****A*****';
+
+let middleMe = (N, X, Y) => N%2 ? X : Y.repeat(N/2)+X+Y.repeat(N/2);
