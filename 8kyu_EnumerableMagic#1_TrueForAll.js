37917a732c62e7ce97ed2c1390087bf30b4bfbe5
Tong Lin <brooklyntechlin@gmail.com>
2021-08-04 05:10:35 -0400

    added 8kyu - Enumerable Magic #1 - True for All

---
+++ 8kyu_EnumerableMagic#1_TrueForAll.js
@@ -0,0 +1,7 @@
+// 8kyu - Enumerable Magic #1 - True for All? 
+
+// Create a method all which takes an array and a predicate (function pointer), and returns true if the predicate returns true for every element in the array. Otherwise, it should return false. If the array is empty, it should return true, since technically nothing failed the test.
+
+// Here's a (Ruby) resource if you get stuck: http://www.rubycuts.com/enum-all
+
+let all = (arr, fun) => arr.every(fun);
